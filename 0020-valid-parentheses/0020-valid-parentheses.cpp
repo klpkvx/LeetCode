a34f56d737f2e24d433e7481b9f7ef5f6066240d
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-04 13:37:14 +0300

    Time: 0 ms (100%), Space: 8 MB (24.96%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        set<char> open = {'(', '{', '['};
+        set<char> closed = {')', '}', ']'};
+        for (char c : s)
+        {
+            if (!st.empty ())
+            {
+                char sym = st.top ();
+                if (c == '(' || c == '[' || c== '{')
+                    st.push (c);
+                else if (sym == '(' && c == ')' || sym == '{' && c == '}' || sym == '[' && c == ']')
+                    st.pop ();
+                else
+                    return false;
+            }
+            else
+            {
+                st.push (c);
+            }
+        }
+        return st.empty ();
+    }
+};
