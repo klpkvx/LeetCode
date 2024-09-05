e0f79b0d58df6b4a22141cae8d50fe21c1f051a6
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-05 17:27:13 +0300

    Time: 11 ms (12.28%), Space: 16.6 MB (94.16%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -5,7 +5,7 @@ public:
         int right = nums.size () - 1;
         while (left <= right)
         {
-            int mid = (left + right) / 2;
+            int mid = (left + right) >> 1;
             if (nums[mid] == target)
                 return true;
 
