870d0271a5a9837aa825ed019b4c23aec6fb793e
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-05 11:57:46 +0300

    Time: 0 ms (100%), Space: 14 MB (42.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -7,16 +7,12 @@ public:
             int mid = (left + right) / 2;
             if (nums[mid] == target)
                 return mid;
-            
-            // left sorted portion:
             if (nums[left] <= nums[mid]){
-
                 if (target > nums[mid] || target < nums[left])
                     left = mid + 1;
                 else
                     right = mid - 1;
             }
-            // right sorted portion: nums[left] > nums[mid]
             else{
                 if (target < nums[mid] || target > nums[right])
                     right = mid - 1;
