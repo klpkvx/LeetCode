b6374073931f17d85bb361f738fe337565d2d7e5
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-06 20:25:09 +0300

    Time: 3 ms (66.47%), Space: 7.9 MB (12.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
+    int fiib (int n, unordered_map<int, int> &data)
+    {
+        if (n == 0 || n == 1)
+            return n;
+
+        if (data.contains (n))
+            return data[n];
+        else
+            data[n] = fiib (n - 1, data) + fiib (n - 2, data);
+
+        return data[n];
+    }
     int fib(int n) {
-        if (n==0)
-            return 0;
-        if (n==1)
-            return 1;
-        return fib (n - 1) + fib (n - 2);
+        unordered_map<int,int> data = { {0, 0}, {1, 1} };
+        fiib (n, data);
+        return data[n];
     }
 };
