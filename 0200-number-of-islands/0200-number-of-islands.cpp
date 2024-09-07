cf73a09e2e94f6585f7f7cb786e557227c5c9b0e
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-07 13:41:08 +0300

    Time: 30 ms (47.85%), Space: 16.1 MB (84.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void dfs (vector<vector<char>> &grid, int i, int j){
-        if (i < 0 || j < 0 || i >= grid.size () || j >= grid[0].size () || grid[i][j] == '2' || grid[i][j] == '0')
+        if (i < 0 || j < 0 || i >= grid.size () || j >= grid[0].size () || grid[i][j] == '0')
             return;
-        
-        grid[i][j] = '2';
+
+        grid[i][j] = '0';
 
         dfs (grid, i + 1, j);
         dfs (grid, i - 1, j);
