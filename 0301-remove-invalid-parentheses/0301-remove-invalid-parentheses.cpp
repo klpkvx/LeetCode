f546f2b1ff05374c7d71853630ae97e0d1c8f14c
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-08 22:14:31 +0300

    Time: 1170 ms (5.08%), Space: 384.8 MB (8.16%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,44 +1,89 @@
 class Solution {
 public:
-
-    void dfs (string &s, int index, int left_removed, int right_removed, int pair, string path, unordered_set<string> &result)
-    {
-        if (index >= s.size ()){
-            if (left_removed == 0 && right_removed == 0 && pair == 0)
-                result.insert (path);
-        }
-        else{
-            if (s[index] == '('){
-                if (left_removed > 0)
-                    dfs (s, index + 1, left_removed - 1, right_removed, pair, path, result);
-                dfs (s, index + 1, left_removed, right_removed, pair + 1, path + s[index], result);
-            }
-            else if (s[index] == ')'){
-                if (right_removed > 0)
-                    dfs (s, index + 1, left_removed, right_removed - 1, pair, path, result);
-                if (pair > 0)
-                    dfs (s, index + 1, left_removed, right_removed, pair - 1, path + s[index], result);
-            }
-            else
-                dfs (s, index + 1, left_removed, right_removed, pair, path + s[index], result);
-        }
-    }
+    unordered_set<string>st;
+    int len;
     vector<string> removeInvalidParentheses(string s) {
-        unordered_set<string> result;
-        int left_removed = 0;
-        int right_removed = 0;
-        for (auto c : s){
-            if (c == '('){
-                left_removed++;
+        len=s.length();
+        vector<string>ans;
+        int open=0,close=0;
+        int c=0;
+        stack<char>stk;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            {
+                stk.push(s[i]);
             }
-            else if (c == ')'){
-                if (left_removed != 0)
-                    left_removed--;
+            else if(s[i]==')')
+            {
+                if(!stk.empty() and stk.top()=='(')
+                {
+                    stk.pop();
+                }
                 else
-                    right_removed++;
+                {
+                    c++;
+                }
             }
         }
-        dfs (s, 0, left_removed, right_removed, 0, "", result);
-        return vector<string> (result.begin (), result.end ());
+        c+=(stk.size());
+        cout<<c<<endl;
+        string str="";
+        fun(s,str,0,c);
+        for(auto x:st)
+        {
+            ans.push_back(x);
+        }
+        if(ans.size()==0)
+            return {""};
+        return ans;
+    }
+    void fun(string &s,string &str,int idx,int &c)
+    {
+        if(str.length()==len-c)
+        {
+            //cout<<str<<endl;
+            if(st.find(str)==st.end())
+            {
+                if(isvalid(str))
+                {
+                    st.insert(str);
+                }
+            }
+            return;
+        }
+        if(idx>=s.length())
+        {
+            return;
+        }
+        str.push_back(s[idx]);
+        fun(s,str,idx+1,c);
+        str.pop_back();
+        fun(s,str,idx+1,c);
+    }
+    bool isvalid(string &s)
+    {
+        stack<char>st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(s[i]);
+            }
+            else if(s[i]==')')
+            {
+                if(!st.empty() and st.top()=='(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if(st.size()>=1)
+            return false;
+        return true;
     }
 };
