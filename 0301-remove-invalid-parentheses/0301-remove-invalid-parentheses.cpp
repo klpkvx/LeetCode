00e0fe75035fecb9dab95048ec490b58c23a4820
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-08 22:17:27 +0300

    Time: 143 ms (47.29%), Space: 11.4 MB (77.59%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,60 +1,53 @@
 class Solution {
 public:
-    void solve(string&s,unordered_set<string>&ans,int open,int close,int bal,int index,string&output){
-        if(index>=s.length()){
-            if(open==0 && close==0 && bal==0)
-                ans.insert(output);
-            return ;
-        }
-        //include exclude
-        if(s[index]!='(' && s[index]!=')'){
-            output.push_back(s[index]);
-            solve(s,ans,open,close,bal,index+1,output);
-            output.pop_back();
+
+    void dfs (string &s, int index, int left_removed, int right_removed, int pair, string &path, unordered_set<string> &result)
+    {
+        if (index >= s.size ()){
+            if (left_removed == 0 && right_removed == 0 && pair == 0)
+                result.insert (path);
         }
         else{
-            //brackets
-            if(s[index]=='('){
-                //include exclude
-                //remove 
-                if(open>0)
-                    solve(s,ans,open-1,close,bal,index+1,output);
-                //dont remove
-                output.push_back('(');
-                solve(s,ans,open,close,bal+1,index+1,output);
-                output.pop_back();
+            if (s[index] == '('){
+                if (left_removed > 0)
+                    dfs (s, index + 1, left_removed - 1, right_removed, pair, path, result);
+                path.push_back (s[index]);
+                dfs (s, index + 1, left_removed, right_removed, pair + 1, path, result);
+                path.pop_back ();
             }
-            else if(s[index]==')'){
-                //remove
-                if(close>0){
-                    solve(s,ans,open,close-1,bal,index+1,output);
+            else if (s[index] == ')'){
+                if (right_removed > 0)
+                    dfs (s, index + 1, left_removed, right_removed - 1, pair, path, result);
+                if (pair > 0){
+                    path.push_back (s[index]);
+                    dfs (s, index + 1, left_removed, right_removed, pair - 1, path, result);
+                    path.pop_back ();
                 }
-                //dont remove
-                //we are at a closing bracket and previously no open bracket was there
-                if(bal>0){
-                    output.push_back(')');
-                    solve(s,ans,open,close,bal-1,index+1,output);
-                    output.pop_back();
-                }
+            }
+            else{
+                path.push_back (s[index]);
+                dfs (s, index + 1, left_removed, right_removed, pair, path, result);
+                path.pop_back ();
             }
         }
     }
     vector<string> removeInvalidParentheses(string s) {
-        unordered_set<string>ans;
-        int invalidOpen=0;
-        int invalidClose=0;
-        int balance=0;
-        int index=0;
-        string output="";
-        for(auto ch:s){
-            if(ch=='(') invalidOpen++;
-            else if(ch==')'){
-            if(invalidOpen>0)
-                invalidOpen--;
-            else invalidClose++;
+        unordered_set<string> result;
+        int left_removed = 0;
+        int right_removed = 0;
+        for (auto c : s){
+            if (c == '('){
+                left_removed++;
+            }
+            else if (c == ')'){
+                if (left_removed != 0)
+                    left_removed--;
+                else
+                    right_removed++;
             }
         }
-        solve(s,ans,invalidOpen,invalidClose,balance,index,output);
-        return vector<string>(ans.begin(),ans.end());
+        string data;
+        dfs (s, 0, left_removed, right_removed, 0, data, result);
+        return vector<string> (result.begin (), result.end ());
     }
 };
