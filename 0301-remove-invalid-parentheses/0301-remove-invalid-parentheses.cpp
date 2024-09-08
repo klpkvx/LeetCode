93aff9ab8b64e5e5b2c9eceed85a0546d6831cb6
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-08 22:14:51 +0300

    Time: 133 ms (52.37%), Space: 11.5 MB (72.84%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,89 +1,60 @@
 class Solution {
 public:
-    unordered_set<string>st;
-    int len;
+    void solve(string&s,unordered_set<string>&ans,int open,int close,int bal,int index,string&output){
+        if(index>=s.length()){
+            if(open==0 && close==0 && bal==0)
+                ans.insert(output);
+            return ;
+        }
+        //include exclude
+        if(s[index]!='(' && s[index]!=')'){
+            output.push_back(s[index]);
+            solve(s,ans,open,close,bal,index+1,output);
+            output.pop_back();
+        }
+        else{
+            //brackets
+            if(s[index]=='('){
+                //include exclude
+                //remove 
+                if(open>0)
+                    solve(s,ans,open-1,close,bal,index+1,output);
+                //dont remove
+                output.push_back('(');
+                solve(s,ans,open,close,bal+1,index+1,output);
+                output.pop_back();
+            }
+            else if(s[index]==')'){
+                //remove
+                if(close>0){
+                    solve(s,ans,open,close-1,bal,index+1,output);
+                }
+                //dont remove
+                //we are at a closing bracket and previously no open bracket was there
+                if(bal>0){
+                    output.push_back(')');
+                    solve(s,ans,open,close,bal-1,index+1,output);
+                    output.pop_back();
+                }
+            }
+        }
+    }
     vector<string> removeInvalidParentheses(string s) {
-        len=s.length();
-        vector<string>ans;
-        int open=0,close=0;
-        int c=0;
-        stack<char>stk;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]=='(')
-            {
-                stk.push(s[i]);
-            }
-            else if(s[i]==')')
-            {
-                if(!stk.empty() and stk.top()=='(')
-                {
-                    stk.pop();
-                }
-                else
-                {
-                    c++;
-                }
+        unordered_set<string>ans;
+        int invalidOpen=0;
+        int invalidClose=0;
+        int balance=0;
+        int index=0;
+        string output="";
+        for(auto ch:s){
+            if(ch=='(') invalidOpen++;
+            else if(ch==')'){
+            if(invalidOpen>0)
+                invalidOpen--;
+            else invalidClose++;
             }
         }
-        c+=(stk.size());
-        cout<<c<<endl;
-        string str="";
-        fun(s,str,0,c);
-        for(auto x:st)
-        {
-            ans.push_back(x);
-        }
-        if(ans.size()==0)
-            return {""};
-        return ans;
-    }
-    void fun(string &s,string &str,int idx,int &c)
-    {
-        if(str.length()==len-c)
-        {
-            //cout<<str<<endl;
-            if(st.find(str)==st.end())
-            {
-                if(isvalid(str))
-                {
-                    st.insert(str);
-                }
-            }
-            return;
-        }
-        if(idx>=s.length())
-        {
-            return;
-        }
-        str.push_back(s[idx]);
-        fun(s,str,idx+1,c);
-        str.pop_back();
-        fun(s,str,idx+1,c);
-    }
-    bool isvalid(string &s)
-    {
-        stack<char>st;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]=='(')
-            {
-                st.push(s[i]);
-            }
-            else if(s[i]==')')
-            {
-                if(!st.empty() and st.top()=='(')
-                {
-                    st.pop();
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-        if(st.size()>=1)
-            return false;
-        return true;
+        solve(s,ans,invalidOpen,invalidClose,balance,index,output);
+        return vector<string>(ans.begin(),ans.end());
     }
 };
