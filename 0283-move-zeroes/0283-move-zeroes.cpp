8a0b7674c105ddbac9bc5955541c1aeffe7cce62
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-09 01:29:47 +0300

    Time: 12 ms (80.03%), Space: 21.9 MB (74.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,12 +4,9 @@ public:
         int ptr = 0;
         for (int i = 0; i < nums.size (); i++){
             if (nums[i] != 0){
-                nums[ptr] = nums[i];
+                swap (nums[ptr], nums[i]);
                 ptr++;
             }
         }
-
-        for (int i = ptr; i < nums.size (); i++)
-            nums[i] = 0;
     }
 };
