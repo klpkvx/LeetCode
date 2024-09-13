25ee6fabcbf30a33c9033b28dc046ca67ad876a3
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-13 10:08:01 +0300

    Time: 9 ms (54.24%), Space: 9.6 MB (36.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
+
+    int max_count (unordered_map<char,int>&data){
+        int max = -1;
+        for (auto &[elem, count] : data)
+            if (count > max)
+                max = count;
+        return max;
+    }
+
     int characterReplacement(string s, int k) {
         unordered_map<char, int> data; // char -> freq;
         int result = 0;
         int left = 0;
         int right = 0;
-        int maxf = 0;
         for (int right = 0; right < s.size (); right++){
+
             data[s[right]]++;
-            maxf = max (maxf, data[s[right]]);
-            while (right - left + 1 - maxf > k){
+            while (right - left + 1 - max_count (data) > k){
                 data[s[left]]--;
                 left++;
             }
