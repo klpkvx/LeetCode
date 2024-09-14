889fefb6f9f2b6698bb93bc938d984fcd70d976a
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-15 01:40:17 +0300

    Time: 19 ms (30.41%), Space: 19.4 MB (97.9%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int value = 0;
+        for (int i = 0; i < nums.size (); i++)
+            value ^= nums[i];
+        return value;
+    }
+};
