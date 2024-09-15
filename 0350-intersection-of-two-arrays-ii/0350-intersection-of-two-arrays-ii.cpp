39b8a083e81b16e96921945ad9b5bb7d2a6d171d
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-15 18:39:16 +0300

    Time: 8 ms (20.65%), Space: 13.7 MB (40.85%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> n1_info; // nums[i] -> count;
-        unordered_map<int, int> n2_info; // nums[i] -> count;
-        unordered_map<int, int> result;
-        for (auto val : nums1)
-            n1_info[val]++;
-        for (auto val : nums2)
-            n2_info[val]++;
-
-        const auto &curr_data = n1_info.size () < n2_info.size () ? n1_info : n2_info;
-        const auto &cmp_data = n1_info.size () < n2_info.size () ? n2_info : n1_info;
-        for (auto &[value, count] : curr_data){
-            auto it = cmp_data.find (value);
-            if (it != cmp_data.end ()){
-                result[value] = min (count, it->second);
+        vector<int> answer;
+        unordered_map<int, int> n1_info; // nums1[i] -> count
+        for (auto value : nums1){
+            n1_info[value]++;
+        }
+        for (int i = 0; i < nums2.size (); i++){
+            int candidate = nums2[i];
+            auto it = n1_info.find (candidate);
+            if (it == n1_info.end ())
+                continue;
+            if (it->second > 0){
+                it->second--;
+                answer.push_back (it->first);
             }
         }
-
-        vector<int> result_arr;
-        for (auto &[value, count] : result){
-            while (count--){
-                result_arr.push_back (value);
-            }
-        }
-        return result_arr;        
+        return answer;
     }
 };
