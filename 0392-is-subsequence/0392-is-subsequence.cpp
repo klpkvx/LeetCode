ecbff2d9bf00394d846f2c046d0413b6f8c3c4d4
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-15 18:44:14 +0300

    Time: 0 ms (100%), Space: 7.8 MB (18.61%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,16 +3,14 @@ public:
     bool isSubsequence(string s, string t) {
         if (s.empty () && t.empty ())
             return true;
-            
         if (s.size () > t.size ())
             return false;
-
-        int sptr = 0;
+        int s_ptr = 0;
         for (int i = 0; i < t.size (); i++){
-            if (sptr < s.size () && t[i] == s[sptr]){
-                sptr++;
+            if (t[i] == s[s_ptr]){
+                s_ptr++;
             }
-            if (sptr == s.size ())
+            if (s_ptr == s.size ())
                 return true;
         }
         return false;
