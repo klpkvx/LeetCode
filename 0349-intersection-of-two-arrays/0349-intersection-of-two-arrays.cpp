f65ae40738c2e1c9c632f1490eb11155aefeb6fb
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-15 12:19:18 +0300

    Time: 2 ms (84.36%), Space: 13.5 MB (48.92%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,19 +3,15 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> result;
         unordered_set<int> n1_unique;
-        unordered_set<int> n2_unique;
         for (int i = 0; i < nums1.size (); i++){
             n1_unique.insert (nums1[i]);
         }
 
         for (int i = 0; i < nums2.size (); i++){
-            n2_unique.insert (nums2[i]);
-        }
-        const auto &container = n1_unique.size () < n2_unique.size () ? n1_unique : n2_unique;
-        const auto &cmp_container = n1_unique.size () < n2_unique.size () ? n2_unique : n1_unique;
-        for (auto num : container){
-            if (cmp_container.contains (num))
-                result.push_back (num);
+            if (n1_unique.contains (nums2[i])){
+                n1_unique.erase (nums2[i]);
+                result.push_back (nums2[i]);
+            }
         }
         return result;
     }
