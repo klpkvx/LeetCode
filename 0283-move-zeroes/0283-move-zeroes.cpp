8d5e04f6a63f8517e721279f0fd75593841c7207
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-15 12:07:46 +0300

    Time: 23 ms (15.74%), Space: 21.9 MB (28.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int ptr = 0;
-        for (int i = 0; i < nums.size (); i++){
+        for (int i = 0; i < nums.size (); i++)
             if (nums[i] != 0){
                 swap (nums[ptr], nums[i]);
                 ptr++;
             }
-        }
+        
     }
 };
