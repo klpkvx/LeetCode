16de57b6d7227eae7338bf26b67eae3aee3d253e
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-15 11:44:42 +0300

    Time: 14 ms (55.74%), Space: 20.7 MB (37.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int sum = 0;
+        int all_sum = 0;
         for (int i= 0; i < nums.size (); i++){
-            sum += nums[i];
+            all_sum += (i + 1) - nums[i];
         }
         int sz = nums.size();
-        return (sz * (sz + 1) >> 1) - sum;
+        return all_sum;
     }
 };
