8a46b180b9787d18e95722979630d3ef57a25ac9
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-18 18:51:56 +0300

    Time: 12 ms (96.24%), Space: 28.7 MB (75.7%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,55 +1,22 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> answer;
-        if (nums[0] >= 0){
-            for (int val : nums)
-                answer.push_back (val * val);
-            return answer;
-        }
-        // nums[0] < 0
-        // find the first occurence positive value in nums;
-        int positive_index = -1;
-        for (int i = 0; i < nums.size (); i++) {
-            if (nums[i] >= 0) {
-                positive_index = i;
-                break;
+        int left = 0;
+        int right = nums.size () - 1;
+        int i = nums.size () - 1;
+        vector<int> answer (nums.size (), 0);
+        while (left <= right) {
+            int l_val = nums[left] * nums[left];
+            int r_val = nums[right] * nums[right];
+            if (r_val > l_val){
+                answer[i] = r_val;
+                right--;
+                i--;
+            } else {
+                answer[i] = l_val;
+                left++;
+                i--;
             }
-        }
-
-        // all values in array are negative
-        if (positive_index == -1){
-            for (int i = nums.size () - 1; i >= 0; i--){
-                answer.push_back (nums[i] * nums[i]);
-            }
-            return answer;
-        }
-        // negative ..... positive .... end
-        int left = positive_index - 1;
-        int right = positive_index;
-        for (int ptr = 0; ptr < nums.size (); ptr++) {
-            int value = 0;
-            if (left >= 0 && right < nums.size ()) {
-                if (abs (nums[left]) < nums[right]){
-                    value = nums[left] * nums[left];
-                    left--;
-                }
-                else {
-                    value = nums[right] * nums[right];
-                    right++;
-                }
-            }
-            else if (left >= 0)
-            {
-                value = nums[left] * nums[left];
-                left--;
-            }
-            else
-            {
-                value = nums[right] * nums[right];
-                right++;
-            }
-            answer.push_back (value);
         }
         return answer;
     }
