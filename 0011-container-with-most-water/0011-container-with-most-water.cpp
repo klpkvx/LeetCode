a63f00adaf0659f42208466d6cc3df27f4b340e6
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-19 01:17:31 +0300

    Time: 46 ms (96.61%), Space: 61.6 MB (75.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,10 +4,10 @@ public:
         int left = 0;
         int right = height.size () - 1;
         int max_area = 0;
-        while (left <= right) { 
+        while (left <= right) {
             int h = min (height[left], height[right]);
-            int width = right - left;
-            max_area = max (max_area, width * h);
+            int w = right - left;
+            max_area = max (max_area, h * w);
             if (height[left] < height[right]) {
                 left++;
             } else {
