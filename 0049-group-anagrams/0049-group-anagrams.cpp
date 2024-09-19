38f0ae67b4a577095cb51e934b41290668c2f8f0
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-19 17:53:17 +0300

    Time: 15 ms (98.79%), Space: 22.9 MB (94.19%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
         unordered_map<string, vector<string>> data;
-        for (int i = 0; i < strs.size (); i++)
-        {
-            string str = strs[i];
-            sort (str.begin (), str.end ());
-            data[str].push_back (strs[i]);
+        for (const string &str : strs) {
+            string tmp = str;
+            sort (tmp.begin (), tmp.end ());
+            if (data.contains (tmp))
+                data[tmp].push_back (str);
+            else
+                data[tmp] = {str};
         }
-
-        vector<string> tmp_res;
-        for (auto &[key, val] : data)
-            ans.push_back (val);
+        vector<vector<string>> ans;
+        for (auto &[str, val] : data) {
+            ans.push_back (std::move (val));
+        }
         return ans;
     }
 };
