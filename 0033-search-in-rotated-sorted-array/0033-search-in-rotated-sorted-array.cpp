ab3ae3721e03d6c84e50b4489563eddaa852a072
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-19 13:39:22 +0300

    Time: 8 ms (10.26%), Space: 13.9 MB (49.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,28 +3,25 @@ public:
     int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size () - 1;
-        while (left <= right)
-        {
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if (nums [mid] == target)
+            if (nums[mid] == target)
                 return mid;
-
-            if (nums[left] <= nums[mid])
-            {
-                if (target > nums[mid] || target < nums[left])
-                    left = mid + 1;
-                else
+            
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
-            }
-            else
-            {
-                if (target < nums[mid] || target > nums[right])
-                    right = mid - 1;
-                else
+                } else {
                     left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-
         return -1;
     }
 };
