3087b77cde4640740cea3008fa01514ce1bf0ecb
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-19 17:34:41 +0300

    Time: 187 ms (24.69%), Space: 95.7 MB (63.42%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.cpp
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int, int>> zipped_arr;
+        auto cmp = [] (const pair<int,int> &left, const pair<int,int> &right){
+            return left.second < right.second;
+        };
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype (cmp)> pq (cmp);
         for (int i = 0; i < nums1.size (); i++) {
-            zipped_arr.push_back ({nums1[i], nums2[i]});
+            pq.push ({nums1[i], nums2[i]});
         }
-
-        sort (zipped_arr.begin (), zipped_arr.end (), [] (const pair<int,int> &left, const pair<int,int> &right){
-            return left.second > right.second;
-        });
-
         auto cmp_elem = [] (const int &left, const int &right) {
             return left > right;
         };
@@ -18,9 +17,9 @@ public:
         priority_queue<int, vector<int>, decltype (cmp_elem)> k_range (cmp_elem);
         long long sum = 0;
         long long max_score = 0;
-        for (const auto &[n1_, n2_] : zipped_arr) {
-            long long n1 = n1_;
-            long long n2 = n2_; // maximized min element in queue 
+        while (!pq.empty ()) {
+            long long n1 = pq.top ().first;
+            long long n2 = pq.top ().second; // maximized min element in queue 
             k_range.push (n1);
             sum += n1;
             if (k_range.size () > k) {
@@ -32,6 +31,7 @@ public:
                 long long curr_score = sum * n2;
                 max_score = max (max_score, curr_score);
             }
+            pq.pop ();
         }
         return max_score;
 
