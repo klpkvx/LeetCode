0a1708eb914273507f021eb0104670ed2c5be78b
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-20 12:07:41 +0300

    Time: 9 ms (47.84%), Space: 20.4 MB (84.58%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,29 +1,27 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     long long val;
+ *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(long long x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(long long x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool is_valid (TreeNode *root, long long left, long long right) {
-        if (!root) {
+    bool solve (TreeNode *root, long long left, long long right) {
+        if (!root)
             return true;
-        }
 
         if (left < root->val && root->val < right) {
-            return is_valid (root->left, left, root->val) && is_valid (root->right, root->val, right);
+            return solve (root->left, left, root->val) && solve (root->right, root->val, right);
         }
-
-        else return false;
+        return false;
     }
 
     bool isValidBST(TreeNode* root) {
-        return is_valid (root, LONG_MIN, LONG_MAX);
+        return solve (root, LONG_MIN, LONG_MAX);
     }
 };
