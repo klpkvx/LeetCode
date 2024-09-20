bed77c2f0fc504277b6fc9231149941e0267cc47
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-20 11:32:20 +0300

    Time: 8 ms (81.07%), Space: 17.8 MB (21.66%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> data; // word -> count
-
-        for (const auto &word : words)
+        unordered_map<string, int> data; // word -> frequency
+        for (const string &word : words) {
             data[word]++;
-        
-        vector<pair<int, string>> tmp_result;
-        for (const auto &[word, count] : data)
-            tmp_result.emplace_back (count, word);
-
-        sort (tmp_result.begin (), tmp_result.end (), [] (const pair<int, string> &left, const pair<int, string> &right) {
-            if (left.first == right.first){
-                return left.second < right.second;
-            }
-            return left.first > right.first;
-        });
-
-        vector<string> result;
-        for (const auto &data : tmp_result){
-            if (k > 0){
-                result.emplace_back (data.second);
-                k--;
-            }
         }
 
+        auto cmp = [] (const pair<int,string> &left, const pair<int, string> &right){
+            if (left.first == right.first) {
+                return left.second > right.second;
+            };
+            return left.first < right.first;
+        };
 
+        priority_queue<pair<int, string>, vector<pair<int,string>>, decltype (cmp)> pq (cmp);
+        for (const auto &[word, freq] : data) {
+            pq.push ({freq, word});
+        }
 
-        return result;
+        vector<string> answer;
+        while (!pq.empty ()) {
+            auto  &elem = pq.top ();
+            if (k-- > 0)
+                answer.push_back (elem.second);
+            else {
+                break;
+            }
+            pq.pop ();
+        }
+        return answer;
     }
 };
