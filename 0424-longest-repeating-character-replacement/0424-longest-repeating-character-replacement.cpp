182fdb837f55dc70ec8bb55ec7754b1d5973e821
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-21 14:21:53 +0300

    Time: 12 ms (40.4%), Space: 9.6 MB (35.89%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
 
-/*
-
-
-
-*/
-    int get_max_freq (unordered_map<char,int> &data) {
-        int mx_freq = 0;
-        for (auto &[sym, val] : data) {
-            mx_freq = max (mx_freq, val);
-        }
-        return mx_freq;
-    }
-
     int characterReplacement(string s, int k) {
         unordered_map<char, int> data; // symbol -> frequency in substring
         int left = 0;
         int right = 0;
         int max_len = 0;
         int max_freq = 0;
-// AAAA: A(1), mx1, A(2), 
         for (int right = 0; right < s.size (); right++) {
             data[s[right]]++;
             max_freq = max (max_freq, data[s[right]]);
