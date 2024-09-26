b22fd720b7f67515b123fe37aa4bfa67cab7f517
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-26 11:33:09 +0300

    Time: 289 ms (22.41%), Space: 157.4 MB (23.99%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-// Найти подмассив у которого сумма чисел равна x. Вернуть минимальную длину. Если такого массива нет, вернуть -1.
     int minOperations(vector<int>& nums, int x) {
-        int target = accumulate (nums.begin (), nums.end (), 0) - x;
-        int sum = 0;
-        int left = 0;
-        int right = 0; 
-        int ans = -1;
-        int sz = nums.size ();
-        for (int right = 0; right < sz; right++) {
-            sum += nums[right];
-            while (left < sz && sum > target) {
-                sum -= nums[left];
-                left++;
-            }
-            if (sum == target) {
-                ans = max (ans, right - left + 1);
-            }
+        int n = nums.size();
+        int totalsum = accumulate(nums.begin(),nums.end(),0);
+        int target = totalsum - x;  //we have to remove x from either end total , we are left with subarray of sum target
+        unordered_map<int,int> mp; //store prefix sums , for finding subarray with sum target
+        mp[0] = -1;         //for subarrays starting from 0 index
+        int presum = 0;
+        int maxsub = INT_MIN;
+        if(x == totalsum){
+            return n;
         }
-        return ans == -1 ? -1 : sz - ans;
+        for(int i=0;i<nums.size();i++){     //prefixsum algo mp: {prefixsum,index}
+            presum += nums[i];
+            if(mp.find(presum-target) != mp.end()){
+                int sub = i - mp[presum-target];     //we have to maximise this , so minimum operations return
+                maxsub = max(maxsub,sub);
+            }
+            mp[presum] = i;         //store prefisum with ending index
+        }
+        if(maxsub == INT_MIN){         //no subarray sum target recognised , return -1
+            return -1;
+        }
+        return (n - maxsub);     //return n - lengthofsubarray to return the number of operations
     }
 };
