91d57ad0b754ae4f87833b03e4a119b15620848d
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-26 11:57:44 +0300

    Time: 84 ms (65.64%), Space: 88.4 MB (29.74%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        unordered_map<int, int> data = {{0, -1}}; // sum -> len
-        int zeroes = 0, ones = 0;
-        int max_len = 0;
+        unordered_map<int, int> data = {{0, -1}}; // diff = non-zeroes - zeroes -> start_index;
+        int non_zeroes = 0;
+        int zeroes = 0;
+        int ans = 0;
         for (int i = 0; i < nums.size (); i++) {
-            if (nums[i] == 0) zeroes++;
-            else ones++;
-            int diff = ones - zeroes;
-            if (data.count (diff)) {
-                max_len = max (max_len, i - data[diff]);
-            } else data[diff] = i;           
+            if (nums[i] == 0) {
+                zeroes++;
+            } else {
+                non_zeroes++;
+            }
+            int diff = non_zeroes - zeroes;
+            if (data.contains (diff)) {
+                ans = max (ans, i - data[diff]);
+            } else 
+                data[diff] = i;
         }
-        return max_len;
+        return ans;
     }
 };
