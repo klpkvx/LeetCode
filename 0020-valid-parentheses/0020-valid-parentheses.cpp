7ca20d88804dbf750b66858f90065a45e1d2247d
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-28 19:20:58 +0300

    Time: 3 ms (40.77%), Space: 7.9 MB (25.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        unordered_map<char, char> brackets {
-             {'(', ')'}, {'{', '}'}, {'[', ']'}
-        };
-        for (int i = 0; i < s.size (); i++){
-            if (brackets.contains (s[i]))
+        for (int i = 0; i < s.size (); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
                 st.push (s[i]);
-            else{
-                if (st.empty ()  ||  brackets[st.top ()] != s[i])
-                    return false;
-                else
-                    st.pop ();
-            }
+            } else if (!st.empty () &&  (st.top () == '(' && s[i] == ')' || st.top () == '{' && s[i] == '}' || st.top () == '[' && s[i] == ']')) {
+                st.pop ();
+            } else 
+                return false;                
         }
         return st.empty ();
     }
