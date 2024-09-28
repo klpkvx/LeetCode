10e0a78321797b8075f848b12b37b7f404f99c47
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-28 23:07:03 +0300

    Time: 28 ms (35.96%), Space: 11 MB (55.32%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -7,33 +7,36 @@ public:
             t_data[t[i]]++;
         }
         int remain_chars = t.size ();
-        vector<int> minW = {0, INT_MAX};
-        for (int left = 0, right = 0; right < s.size (); right++) {
-            char letter = s[right];
+        vector<int> minw = {0, INT_MAX};
+        for (int j = 0, i = 0; i < s.size (); i++) {
+            char letter = s[i];
             if (t_data.find (letter) != t_data.end () && t_data[letter] > 0) {
                 remain_chars--;
             }
             t_data[letter]--;
             if (remain_chars == 0) {
-                while (left < s.size ()) {
-                    char ch = s[left];
-                    if (t_data.find (ch) != t_data.end () && t_data[ch] == 0) {
+                // collapse window
+                while (true) {
+                    char lt = s[j];
+                    if (t_data.find (lt) != t_data.end () && t_data[lt] == 0) {
                         break;
                     }
-                    t_data[ch]++;
-                    left++;
+                    t_data[lt]++;
+                    j++;
                 }
 
-                if (right - left < minW[1] - minW[0]) {
-                    minW[0] = left;
-                    minW[1] = right;
+                if (i - j < minw[1] - minw[0]) {
+                    minw[0] = j;
+                    minw[1] = i;
                 }
 
                 remain_chars++;
-                t_data[s[left]]++;
-                left++;
+                t_data[s[j]]++;
+                j++;
             }
         }
-        return minW[1] >= s.size () ? "" : s.substr (minW[0], minW[1] - minW[0] + 1);
+        if (minw[1] >= s.size ())
+            return "";
+        return s.substr (minw[0], minw[1] - minw[0] + 1);
     }
 };
