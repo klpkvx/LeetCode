0a683af48c168b76f49a15bca00f042338147c9f
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-28 14:14:54 +0300

    Time: 39 ms (69.47%), Space: 58 MB (85.95%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -3,12 +3,13 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int i = 0, j = 0;
         for (j = 0; j < nums.size (); j++) {
-            if (nums[j] == 0) k--;                
+            if (nums[j] == 0) k--;
+
             if (k < 0) {
-                if (nums[i] == 0) {
+                 if (nums[i] == 0) {
                     k++;
-                }
-                i++;
+                 }
+                 i++;
             }
         }
         return j - i;
