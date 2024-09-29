499443d7cfa00bac5172edb6871444e98ad206b1
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 03:24:23 +0300

    Time: 136 ms (54.71%), Space: 11.4 MB (80.34%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,53 +1,57 @@
 class Solution {
 public:
-
-    void dfs (string &s, int index, int left_removed, int right_removed, int pair, string &path, unordered_set<string> &result)
-    {
-        if (index >= s.size ()){
-            if (left_removed == 0 && right_removed == 0 && pair == 0)
-                result.insert (path);
+    void solve (string &s, int index, int left_removed, int right_removed, int pairs, string &path,unordered_set<string> &answer){
+        if (index == s.size ()) {
+            if (left_removed == 0 && right_removed == 0 && pairs == 0) {
+                answer.insert (path);
+            }
+            return;
         }
-        else{
-            if (s[index] == '('){
-                if (left_removed > 0)
-                    dfs (s, index + 1, left_removed - 1, right_removed, pair, path, result);
-                path.push_back (s[index]);
-                dfs (s, index + 1, left_removed, right_removed, pair + 1, path, result);
+
+        char sym = s[index];
+        if (sym != '(' && sym != ')') {
+            path.push_back (sym);
+            solve (s, index + 1, left_removed, right_removed, pairs, path, answer);
+            path.pop_back ();
+        }
+        else {
+            if (sym == '(') {
+                if (left_removed > 0) {
+                    solve (s, index + 1, left_removed - 1, right_removed, pairs, path, answer);
+                }
+                path.push_back (sym);
+                solve (s, index + 1, left_removed, right_removed, pairs + 1, path, answer);
                 path.pop_back ();
             }
-            else if (s[index] == ')'){
-                if (right_removed > 0)
-                    dfs (s, index + 1, left_removed, right_removed - 1, pair, path, result);
-                if (pair > 0){
-                    path.push_back (s[index]);
-                    dfs (s, index + 1, left_removed, right_removed, pair - 1, path, result);
+            if (sym == ')') {
+                if (right_removed > 0) {
+                    solve (s, index + 1, left_removed, right_removed - 1, pairs, path, answer);
+                }
+                if (pairs > 0) {
+                    path.push_back (sym);
+                    solve (s, index + 1, left_removed, right_removed, pairs - 1, path, answer);
                     path.pop_back ();
                 }
             }
-            else{
-                path.push_back (s[index]);
-                dfs (s, index + 1, left_removed, right_removed, pair, path, result);
-                path.pop_back ();
-            }
         }
     }
+
     vector<string> removeInvalidParentheses(string s) {
-        unordered_set<string> result;
+        unordered_set<string> answer;
         int left_removed = 0;
         int right_removed = 0;
-        for (auto c : s){
-            if (c == '('){
+        for (auto c : s) {
+            if (c == '(')
                 left_removed++;
-            }
-            else if (c == ')'){
+            if (c == ')') {
                 if (left_removed != 0)
                     left_removed--;
                 else
                     right_removed++;
             }
         }
-        string data;
-        dfs (s, 0, left_removed, right_removed, 0, data, result);
-        return vector<string> (result.begin (), result.end ());
+        string path;
+        solve (s, 0, left_removed, right_removed, 0, path, answer);
+        return vector<string> (answer.begin (), answer.end ());
     }
 };
