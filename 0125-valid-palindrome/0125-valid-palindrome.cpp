822a9a3c4b1702fa6d7fd2e4eeb548a2fa4909b0
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 17:43:44 +0300

    Time: 6 ms (45.85%), Space: 8.9 MB (54.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,20 +3,20 @@ public:
     bool isPalindrome(string s) {
         int left = 0;
         int right = s.size () - 1;
-
-        while (left <= right){
+        while (left <= right) {
             while (left < s.size () && !isalnum (s[left]))
                 left++;
-            while (right < s.size () && !isalnum (s[right]))
+            while (right >= 0 && !isalnum (s[right]))
                 right--;
-
-            if (left > s.size () - 1 || right > s.size () - 1)
+            if (left >= s.size () || right < 0)
                 return true;
-
-            if (tolower(s[left]) != tolower(s[right]))
+                
+            if (tolower (s[left]) != tolower (s[right])) {
                 return false;
-            left++;
-            right--;
+            } else {
+                left++;
+                right--;
+            }
         }
         return true;
     }
