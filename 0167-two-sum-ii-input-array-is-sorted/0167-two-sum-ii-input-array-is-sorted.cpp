c960fe20aeefe11d6622abaf7f97159bce46cf36
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 19:17:42 +0300

    Time: 7 ms (82.48%), Space: 18.3 MB (13.19%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,10 +3,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size () - 1;
-        while (left < right) { 
-            if (nums[left] < target - nums[right]) {
+        while (left <= right) {
+            int sum = nums[left] + nums[right];
+            if (sum < target) {
                 left++;
-            } else if (nums[left] > target - nums[right]) {
+            } else if (sum > target) {
                 right--;
             } else {
                 return {left + 1, right + 1};
