243d4c12f5e4ead67a17cbcd79255549bd87c728
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 16:57:14 +0300

    Time: 3 ms (69.55%), Space: 11.2 MB (61.16%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string max_pref = strs[0];
+        for (int i = 1; i < strs.size (); i++) {
+            string &curr_str = strs[i];
+            curr_str.resize (min (curr_str.size (), max_pref.size ()));
+            for (int j = 0; j < max_pref.size (); j++) {
+                if (curr_str[0] != max_pref[0])
+                    return "";
+                if (curr_str[j] != max_pref[j]) {
+                    max_pref = max_pref.substr (0, j);
+                    break;
+                }
+            }
+        }
+        return max_pref;   
+    }
+};
