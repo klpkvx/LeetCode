c6bc44cf28890dd9697d0fef5c4613ddaa41516a
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 15:20:54 +0300

    Time: 12 ms (22.83%), Space: 21.4 MB (59.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
 // 1 2 3 4 
     int removeDuplicates(vector<int>& nums) {
-        int ptr = 0;
-        for (int i = 0; i < nums.size (); i++) {
-            if (nums[ptr] == nums[i]) {
-                continue;
-            } else {
-                nums[++ptr] = nums[i];
+        int ptr = 1;
+        for (int i = 1; i < nums.size (); i++) {
+            if (nums[i - 1] != nums[i]) {
+                nums[ptr++] = nums[i];
             }
         }
-        return ptr + 1;
+        return ptr;
     }
 };
