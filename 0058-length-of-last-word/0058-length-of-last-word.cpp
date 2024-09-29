5d304c1b8b22c8821e0f4b6c5ae7b69a7e8b2a84
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 16:45:56 +0300

    Time: 4 ms (47.32%), Space: 7.9 MB (66.48%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int len = 0;
-        int word_len = 0;
-        for (int i = s.size () - 1; i >= 0;) {
-            while (i >= 0 && s[i] == ' ')
-                i--;
+        int i = s.size () - 1;
+        while (i >= 0 && s[i] == ' ')
+            i--;
 
-            while (i >= 0 && s[i--] != ' '){
-                len++;
-                word_len = len;
-            }
-            if (word_len != 0)
-                break;
+        while (i >= 0 && s[i--] != ' '){
+            len++;
         }
-        return word_len;
+        return len;
     }
 };
