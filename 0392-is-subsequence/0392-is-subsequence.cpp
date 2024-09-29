2449b748f72d3d6c267b9fa24041522eef031a80
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 19:14:15 +0300

    Time: 0 ms (100%), Space: 7.7 MB (87.48%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if (s.empty () && t.empty ())
-            return true;
-        if (s.size () > t.size ())
-            return false;
-        int s_ptr = 0;
-        for (int i = 0; i < t.size (); i++){
-            if (t[i] == s[s_ptr]){
-                s_ptr++;
-            }
-            if (s_ptr == s.size ())
+        int ptr = 0;
+
+        for (int i = 0; i < t.size (); i++) {
+            if (ptr == s.size ())
                 return true;
+            if (s[ptr] == t[i])
+                ptr++;
         }
-        return false;
+        return ptr == s.size ();
     }
 };
