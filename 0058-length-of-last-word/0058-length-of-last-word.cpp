349176c38dec19a5bda7155aa2be39c19e862439
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 16:44:45 +0300

    Time: 0 ms (100%), Space: 8 MB (17.67%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,14 +3,16 @@ public:
     int lengthOfLastWord(string s) {
         int len = 0;
         int word_len = 0;
-        for (int i = 0; i < s.size ();) {
-            while (i < s.size () && s[i] == ' ')
-                i++;
-            len = 0;    
-            while (i < s.size () && s[i++] != ' '){
+        for (int i = s.size () - 1; i >= 0;) {
+            while (i >= 0 && s[i] == ' ')
+                i--;
+
+            while (i >= 0 && s[i--] != ' '){
                 len++;
                 word_len = len;
             }
+            if (word_len != 0)
+                break;
         }
         return word_len;
     }
