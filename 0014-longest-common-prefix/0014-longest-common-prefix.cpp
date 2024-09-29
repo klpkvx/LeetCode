b5e0645febd91cde5228fa6e7c1033da426f530c
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 16:57:57 +0300

    Time: 7 ms (26.97%), Space: 11.1 MB (91.9%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,8 +6,6 @@ public:
             string &curr_str = strs[i];
             curr_str.resize (min (curr_str.size (), max_pref.size ()));
             for (int j = 0; j < max_pref.size (); j++) {
-                if (curr_str[0] != max_pref[0])
-                    return "";
                 if (curr_str[j] != max_pref[j]) {
                     max_pref = max_pref.substr (0, j);
                     break;
