51682864cc9c1232a5248ddcb28837dfd6a0d680
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-09-29 19:47:30 +0300

    Time: 12 ms (49.09%), Space: 13 MB (66.04%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> nums = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-        };
-        int result = 0;
-        for (int i = 0; i < s.size (); i++){
 
-            if (i + 1 < s.size () && nums[s[i]] < nums[s[i+1]]){
-                result -= nums[s[i]];
-            }
+// IX
+// 
+    int romanToInt(string s) {
+        unordered_map<char, int> roman_to_i = { 
+            {'I', 1}, 
+            {'V', 5}, 
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+         };
+         int answer = 0;
+         for (int i = 0; i < s.size (); i++) {
+            if (i + 1 < s.size () && roman_to_i[s[i]] < roman_to_i[s[i + 1]])
+                answer -= roman_to_i[s[i]];
             else
-                result += nums[s[i]];
-        }
-    return result;
+                answer += roman_to_i[s[i]];
+         }
+         return answer;
     }
 };
-
-
