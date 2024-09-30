30369686e0abd0b4dc7a3bd1097c5e6255814d64
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-01 01:37:58 +0300

    Time: 28 ms (80.09%), Space: 23.2 MB (40.15%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    unordered_map<string, unsigned int> map;
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> result;
+        unsigned int length = words[0].size();
+
+        map.clear();
+        for (const string& word : words)
+            map[word]++;
+
+        for (unsigned int offset = 0; offset < length; ++offset) {
+            unsigned int size = 0;
+            unordered_map<string, unsigned int> seen;
+            for (unsigned int i = offset; i + length <= s.size(); i += length) {
+                string sub = s.substr(i, length);
+
+                auto itr = map.find(sub);
+                if (itr == map.end()) {
+                    seen.clear();
+                    size = 0;
+                    continue;
+                }
+
+                ++seen[sub];
+                ++size;
+                while (seen[sub] > itr->second) {
+                    string first = s.substr(i - (size - 1) * length, length);
+                    --seen[first];
+                    --size;
+                }
+                
+                if (size == words.size())
+                    result.push_back(i - (size - 1) * length);
+            }
+        }
+
+        return result;
+    }
+};
