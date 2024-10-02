183bd699c8043d7edda454df5530d13e49428346
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-02 23:35:14 +0300

    Time: 139 ms (29.36%), Space: 76.2 MB (85.63%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> data; // elem -> index;
-        for (int i = 0; i < nums.size (); i++) {
-            int curr = nums[i];
-            if (data.contains (curr) && abs (data[curr] - i) <= k)
+        unordered_set<int> data;
+        int left = 0;
+        for (int right = 0; right < nums.size (); right++) {
+            if (data.contains (nums[right]))
                 return true;
-            data[curr] = i;
+            data.insert (nums[right]);
+            if (right - left == k) {
+                data.erase (nums[left]);
+                left++;
+            }
         }
         return false;
     }
