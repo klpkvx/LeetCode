99a66d3811c26c9678adf941a77dbaeb636758fc
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-02 23:05:00 +0300

    Time: 0 ms (100%), Space: 14.4 MB (21.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> data; // nums[i] -> i
-        for (int i = 0; i < nums.size (); i++) { 
-                if (!data.contains (target - nums[i]))
-                    data[nums[i]] = i;
-                else
-                    return {data[target - nums[i]], i};
+        unordered_map<int, int> data; // nums[i] -> index
+        for (int i = 0; i < nums.size (); i++) {
+            int look_for_val = target - nums[i];
+            if (data.contains (look_for_val)) {
+                return {data[look_for_val], i};
+            }
+            data[nums[i]] = i;
         }
         return {};
     }
