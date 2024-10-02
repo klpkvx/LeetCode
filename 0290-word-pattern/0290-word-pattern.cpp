286cab084031f55f129f37cb990187e1bea57bad
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-02 22:43:48 +0300

    Time: 0 ms (100%), Space: 8.2 MB (45.3%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        unordered_map<char, int> p_data; // sym -> freq
-        unordered_map<string, int> s_data; // word -> freq
-        istringstream in(s); 
-        string word;
-        int i = 0;
-        for (; in >> word; i++) {
-            if (i == pattern.size () || p_data[pattern[i]] != s_data[word])
-                return false;
-            p_data[pattern[i]] = i + 228;
-            s_data[word] = i + 228;
+        vector<string> words;
+        for (int i = 0; i < s.size (); i++) {
+            string curr_word;
+            while (i < s.size () && s[i] != ' ')
+                curr_word.push_back (s[i++]);
+            if (!curr_word.empty ())
+                words.push_back (std::move (curr_word)); 
         }
-        return i == pattern.size ();
-    }
+
+        if (pattern.size () != words.size ())
+            return false;
+        int sz = pattern.size ();
+        unordered_map<char, string> p_map;
+        unordered_map<string, char> w_map;
+        for (int i = 0; i < sz; i++) {
+            const string& word = words[i];
+            char letter = pattern[i];
+            if (p_map.contains (letter) && p_map[letter] != word)
+                return false;
+            if (w_map.contains (word) && w_map[word] != letter)
+                return false;
+            p_map[letter] = word;
+            w_map[word] = letter;
+        }
+        return true;
+    } 
 };
