d058e31b328e456c8e61db18e5f371c6fb736f22
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-02 22:21:31 +0300

    Time: 4 ms (10.48%), Space: 8.2 MB (45.34%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, int> p_data; // sym -> freq
+        unordered_map<string, int> s_data; // word -> freq
+        istringstream in(s); 
+        string word;
+        int i = 0;
+        for (; in >> word; i++) {
+            if (i == pattern.size () || p_data[pattern[i]] != s_data[word])
+                return false;
+            p_data[pattern[i]] = i + 1;
+            s_data[word] = i + 1;
+        }
+        return i == pattern.size ();
+    }
+};
