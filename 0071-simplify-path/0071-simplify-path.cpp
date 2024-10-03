fca2148d8d22470f201e25c2df5c3adf2e28c3ed
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-03 17:45:16 +0300

    Time: 3 ms (86.97%), Space: 11.8 MB (44.94%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        string ans;
         stack<string> st;
-        string str;
         for (int i = 0; i < path.size (); i++) {
             if (path[i] == '/')
                 continue;
-            
             string tmp;
-            while (i < path.size () && path[i] != '/'){
-                tmp.push_back (path[i++]);
-            }
-
-            if (tmp == ".."){
+            while (i < path.size () && path[i] != '/')
+                tmp += path[i++];
+            if (tmp == ".")
+                continue;
+            else if (tmp == "..") {
                 if (!st.empty ())
                     st.pop ();
-            } else if (tmp == ".") {
-                continue;
-            } else {
-                st.push (tmp);
             }
+            else
+                st.push (tmp);
         }
-
         while (!st.empty ()) {
-            str = "/" + st.top () + str;
+            ans = "/" + st.top () + ans;
             st.pop ();
         }
-        if (str.empty ()) {
-            return "/";
-        }
-        return str;
+        if (ans.empty ()) return "/";
+        return ans;
     }
 };
