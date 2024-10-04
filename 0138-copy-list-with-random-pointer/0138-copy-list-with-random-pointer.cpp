1b3a0c7325f366f8f19ede16faaefa4116a2f81e
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-04 16:29:24 +0300

    Time: 7 ms (54.13%), Space: 15.2 MB (19.96%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,22 +17,18 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node dummy (0);
-        Node *new_head = &dummy;
         Node *tmp_head = head;
         unordered_map<Node *, Node *> data;
         while (head) {
             data[head] = new Node (head->val);
             head = head->next;
         }
-
         head = tmp_head;
         while (head) {
             data[head]->next = data[head->next];
             data[head]->random = data[head->random];
             head = head->next;
         }
-        
         return data[tmp_head];
     }
 };
