38ee7d70016625f6d59748ca4f6df80ba06b77a3
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-04 15:14:37 +0300

    Time: 7 ms (85.12%), Space: 10.9 MB (21.84%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,21 +8,23 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode* head) {
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
         if (!head || !head->next)
             return false;
 
-        ListNode *slow = head;
-        ListNode *fast = head;
-        while (slow && fast)
-        {
+        while (fast) {
             slow = slow->next;
             if (fast->next)
                 fast = fast->next->next;
             else
                 fast = fast->next;
-            if (slow == fast) return true;
+
+            if (fast == slow)
+                return true;
         }
+
         return false;
     }
 };
