a28a3b652fd3750660ee91bd97681117105ba8ab
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-04 13:31:23 +0300

    Time: 14 ms (34.42%), Space: 20.7 MB (76.07%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back (newInterval);
-        sort (intervals.begin (), intervals.end ());
         vector<vector<int>> answer;
-        answer.push_back(intervals[0]);
-        for (int i = 1; i < intervals.size (); i++) {
-            if (intervals[i][0] <= answer.back ()[1])
-                answer.back ()[1] = max (answer.back ()[1], intervals[i][1]);
-            else
-                answer.push_back (intervals[i]);
+        int i = 0;
+
+        while (i < intervals.size () && intervals[i][1] < newInterval[0])
+            answer.push_back (intervals[i++]);
+
+        while (i < intervals.size () && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min (intervals[i][0], newInterval[0]);
+            newInterval[1] = max (intervals[i][1], newInterval[1]);
+            i++;
         }
+        
+        answer.push_back (newInterval);
+
+        while (i < intervals.size ()) {
+            answer.push_back (intervals[i++]);
+        }
+
         return answer;
     }
 };
