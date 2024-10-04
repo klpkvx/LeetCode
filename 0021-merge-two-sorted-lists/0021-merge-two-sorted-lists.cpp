300bba7a9e983be52fcda9a1e8caae687bd2c19d
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-04 15:44:53 +0300

    Time: 3 ms (86%), Space: 19.6 MB (84.27%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,23 +11,37 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy;
-        ListNode *head = &dummy;
+        if (!list1)
+            return list2;
+
+        if (!list2)
+            return list1;
+
+        ListNode *head = nullptr;
+        if (list1->val < list2->val) {
+            head = list1;
+            list1 = list1->next;
+        } else {
+            head = list2;
+            list2 = list2->next;
+        }
+
+        ListNode *ans_head = head;
         while (list1 && list2) {
             if (list1->val < list2->val) {
-                head->next = new ListNode (list1->val);
+                head->next = list1;
                 list1 = list1->next;
             } else {
-                head->next = new ListNode (list2->val);
+                head->next = list2;
                 list2 = list2->next;
             }
             head = head->next;
         }
-        if (list1) {
+
+        if (list1)
             head->next = list1;
-        } else {
+        else if (list2)
             head->next = list2;
-        }
-        return dummy.next;
+        return ans_head;        
     }
 };
