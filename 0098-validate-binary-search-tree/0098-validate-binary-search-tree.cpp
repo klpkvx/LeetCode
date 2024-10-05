6a232f36d647157bc44ebd7d60152198717d60ea
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-06 00:46:39 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,24 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool solve (TreeNode *root, long long left_value, long long right_value) {
+    bool solve (TreeNode *root, int left, int right) {
         if (!root)
             return true;
-
-        if (left_value < root->val && root->val < right_value) {
-            return solve (root->left, left_value, root->val) && solve (root->right, root->val, right_value);
+      
+        if (left < root->val && root->val < right) {
+            return solve (root->left, left, root->val) && solve (root->right, root->val, right);
         }
-        
         return false;
     }
 
     bool isValidBST(TreeNode* root) {
-        return solve (root, LONG_MIN, LONG_MAX);
+        return solve (root, INT_MIN, INT_MAX);
     }
 };
