1c2f8e66b666870950ec779ee1ba57346dba14b4
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-06 03:12:08 +0300

    Time: 4 ms (36.11%), Space: 12.2 MB (97.16%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -7,7 +7,7 @@ public:
         int left = 0;
         int right = nums.size () - 1;
         while (left <= right) {
-            int mid = (left + right) >> 1;
+            int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
                 left = mid + 1;
             } else if (nums[mid] > target) {
