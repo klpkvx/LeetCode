7f57e6b7f3ed77093e06dab12fe1e0aeecd9022e
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-07 23:53:59 +0300

    Time: 79 ms (46.71%), Space: 70.3 MB (99.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        long long mx_sum = INT_MIN;
-        long long tmp = 0;
-        for (int i = 0; i < nums.size (); i++) {
-            tmp += nums[i];
-            if (tmp > mx_sum) {
-                mx_sum = tmp;
-            }
-            if (tmp <= 0) {
-                tmp = 0;
-            }
+        int maxsum = nums[0];
+        int currmax = nums[0];
+        for (int i = 1; i < nums.size (); i++) {
+            currmax = max (currmax + nums[i], nums[i]);
+            maxsum = max (maxsum, currmax);
         }
-        return mx_sum;
+        return maxsum;
     }
 };
