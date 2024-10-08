ce0623546b713d100f952c461789384893228701
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-08 23:34:01 +0300

    Time: 3 ms (56.85%), Space: 12.9 MB (49.11%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -3,12 +3,11 @@ public:
     int findMin(vector<int>& nums) {
         int left = 0;
         int right = nums.size () - 1;
-        while (left < right){
-            int mid = (left + right) / 2;
-
-            if (nums[mid] > nums[right])
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
                 left = mid + 1;
-            else
+            }  else
                 right = mid;
         }
         return nums[left];
