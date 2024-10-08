05af21b64fd706219dc8c0e4ad8d0187856de274
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-09 00:14:31 +0300

    Time: 159 ms (6.36%), Space: 125.9 MB (90.41%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> answer;
+        int equal = 0;
+        int low = 0;
         for (int i = 0; i < nums.size (); i++) {
             if (nums[i] < pivot)
-                answer.push_back (nums[i]);
+                low++;
+            else if (nums[i] == pivot)
+                equal++;
         }
-        for (int i = 0; i < nums.size (); i++) {
-            if (nums[i] == pivot)
-                answer.push_back (nums[i]);
-        }
-        for (int i = 0; i < nums.size (); i++) {
-            if (nums[i] > pivot)
-                answer.push_back (nums[i]);
+        int i = 0, j = low, k = equal + low;
+        vector<int> answer (nums.size ());
+        for (int elem : nums) {
+            if (elem < pivot) {
+                answer[i++] = elem;
+            } else if (elem == pivot) {
+                answer[j++] = elem;
+            } else {
+                answer[k++] = elem;
+            }
         }
         return answer;
     }
