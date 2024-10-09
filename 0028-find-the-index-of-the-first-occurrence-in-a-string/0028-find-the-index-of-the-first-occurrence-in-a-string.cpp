3527f1654b1085bef9c2f52221b89829e1edded7
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-09 22:27:05 +0300

    Time: 0 ms (100%), Space: 7.7 MB (88.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,15 +5,13 @@ public:
         for (int i = 0; i < haystack.size ();) {
             int start = i;
             int ptr = 0;
-            if (needle[ptr] == haystack[i]) 
-                while (ptr < needed_size && i < haystack.size () && needle[ptr] == haystack[i]) {
+            while (ptr < needed_size && i < haystack.size () && needle[ptr] == haystack[i]) {
                 ptr++;
                 i++;
-                if (ptr == needed_size)
-                    return start;
-                }
-            if (ptr < needed_size)
-                i = start + 1;
+            }
+            if (ptr == needed_size)
+                return start;
+            i = start + 1;
         }
         return -1;
     }
