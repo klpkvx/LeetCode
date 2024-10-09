9e2b4ba3ce1c84a8e1e1348d516e37c32375342c
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-10 02:53:00 +0300

    Time: 10 ms (83.58%), Space: 19.5 MB (66.84%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
+    int singleNumber(vector<int>& nums) {        ios::sync_with_stdio(false);
+
         return accumulate (nums.begin (), nums.end (), int{}, [] (int l, int v) { return l ^ v; });
     }
 };
