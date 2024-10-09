8d55cfced80190637dba26e60c5542be53ec066c
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-09 18:44:39 +0300

    Time: 170 ms (81.5%), Space: 141.9 MB (33.08%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    struct para {
+        int index = 0;
+        int sum = 0;
+        para (int su, int ind) : sum (su), index (ind) {}
+    };
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> result;
+        auto cmp = [] (para &lhs, para &rhs) {
+            return !(lhs.sum < rhs.sum); // at the top pq is smallest sum pair
+        };
+        priority_queue<para, vector<para>, decltype(cmp)> pq (cmp);
+        for (int i = 0; i < nums1.size (); i++) {
+            pq.push (para (nums1[i] + nums2[0], 0));
+        }
+
+        while (k > 0 && !pq.empty ()) {
+            const para &p = pq.top ();
+            int sum = p.sum;
+            int pos = p.index;
+            result.push_back ({sum - nums2[pos], nums2[pos]});
+            pq.pop ();
+            if (pos + 1 < nums2.size ()) {
+                // calculate sum of the next pair by subtracting the current second element from the sum
+                // and adding the next element in nums2
+                pq.push (para (sum - nums2[pos] + nums2[pos + 1], pos + 1));
+            }
+            k--;
+        }
+        return result;
+    }
+}; 
