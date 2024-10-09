dd7131ffd6ac9c19d34c632d3e48870f29625c2d
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-09 22:07:01 +0300

    Time: 8 ms (78.52%), Space: 13.9 MB (45.6%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    //s[i * numrows + j]
+    // abcdefghi -> 1234 321 23
+    string convert(string s, int numrows) {
+        if (numrows == 1 || s.size () < numrows)
+            return s;
+        vector<string> data (numrows, "");
+        int idx = 0; // for numrows
+        int d = 1;
+        for (char c : s) {
+            data[idx].push_back (c);
+            if (idx == 0)
+                d = 1;
+            else if (idx == numrows - 1)
+                d = -1;
+            idx += d;
+        }
+        string answer;
+        for (auto & str : data)
+            answer += str;
+        return answer;
+    }
+};
