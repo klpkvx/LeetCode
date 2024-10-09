ccbb2febcafbc99bc8897d82eb67a4dc286afa7f
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-09 20:05:46 +0300

    Time: 7 ms (34.74%), Space: 9 MB (72.17%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string answer;
+        for (int i = s.size () - 1; i >= 0; i--) {
+            string tmp_word;
+            while (i >= 0 && s[i] != ' ')
+                tmp_word.push_back (s[i--]);
+
+            if (!tmp_word.empty ()) {
+                reverse (tmp_word.begin (), tmp_word.end ());
+                answer += answer.empty () ? tmp_word : " " + tmp_word;
+            }            
+        }
+        return answer;
+    }
+};
