b0c73b5c73e8cb39d48360a1f9f5e792969c19f5
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-10 02:52:24 +0300

    Time: 18 ms (34.68%), Space: 19.5 MB (87.05%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        return accumulate (nums.begin (), nums.end (), 0, [] (int l, int v) { return l ^ v; });
+        return accumulate (nums.begin (), nums.end (), int{}, [] (int l, int v) { return l ^ v; });
     }
 };
