f29bc16010a9d7c213ce7ec0d8f65439f1e9c304
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-10 17:40:55 +0300

    Time: 0 ms (100%), Space: 10.7 MB (77.29%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,9 +3,9 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int ptr = 0;
         for (int i = 0; i < nums.size (); i++) {
-            if (nums[i] != val) {
-                nums[ptr++] = nums[i];
-            }
+            if (nums[i] == val)
+                continue;
+            nums[ptr++] = nums[i];
         }
         return ptr;
     }
