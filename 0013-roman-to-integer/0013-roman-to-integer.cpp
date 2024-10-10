9529feb1cc26c544bb108ea316cd741e7324b74a
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-10 17:10:16 +0300

    Time: 16 ms (27.35%), Space: 13 MB (65.19%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-
-// IX
-// 
     int romanToInt(string s) {
-        unordered_map<char, int> roman_to_i = { 
-            {'I', 1}, 
-            {'V', 5}, 
+        int answer = 0;
+        unordered_map<char, int> roman_to_int = {
+            {'I', 1},
+            {'V', 5},
             {'X', 10},
             {'L', 50},
             {'C', 100},
             {'D', 500},
             {'M', 1000}
-         };
-         int answer = 0;
-         for (int i = 0; i < s.size (); i++) {
-            if (i + 1 < s.size () && roman_to_i[s[i]] < roman_to_i[s[i + 1]])
-                answer -= roman_to_i[s[i]];
-            else
-                answer += roman_to_i[s[i]];
-         }
-         return answer;
+        }; // s[i] -> value
+        
+        for (int i = 0; i < s.size (); i++) {
+            if (i + 1 < s.size () && roman_to_int[s[i]] < roman_to_int[s[i + 1]]) {
+                answer -= roman_to_int[s[i]];
+            } else {
+                answer += roman_to_int[s[i]];
+            }
+        }
+        return answer;
     }
 };
