d5a64124615328221a8501df7abf40c8a014243a
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-10 13:58:08 +0300

    Time: 26 ms (65.83%), Space: 38.4 MB (84.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefix_prod (nums.size () + 1, 1);
-        vector<int> suffix_prod (nums.size () + 1, 1);
-        for (int i = 1; i < nums.size (); i++) {
-            prefix_prod[i] = prefix_prod[i - 1] * nums[i - 1];
+        vector<int> ans (nums.size (), 1);
+        int left = 1;
+        for (int i = 0; i < nums.size (); i++) {
+            ans[i] *= left;
+            left *= nums[i];
         }
-
-        for (int j = nums.size () - 2; j >= 0; j--) {
-            suffix_prod[j] = suffix_prod[j + 1] * nums[j + 1];
+        int right = 1;
+        for (int j = nums.size () - 1; j >= 0; j--) {
+            ans[j] *= right;
+            right *= nums[j];
         }
-        vector<int> answer;
-        for (int i =  0; i < nums.size (); i++) {
-            answer.push_back (prefix_prod[i] * suffix_prod[i]);
-        }
-        return answer;
+        return ans;
     }
 };
 
 
+// OK, but it used memory
+        // vector<int> prefix_prod (nums.size () + 1, 1);
+        // vector<int> suffix_prod (nums.size () + 1, 1);
+        // for (int i = 1; i < nums.size (); i++) {
+        //     prefix_prod[i] = prefix_prod[i - 1] * nums[i - 1];
+        // }
+
+        // for (int j = nums.size () - 2; j >= 0; j--) {
+        //     suffix_prod[j] = suffix_prod[j + 1] * nums[j + 1];
+        // }
+        // vector<int> answer;
+        // for (int i =  0; i < nums.size (); i++) {
+        //     answer.push_back (prefix_prod[i] * suffix_prod[i]);
+        // }
+        // return answer;
 // TLE
         // vector<int> answer (nums.size (), 1);
         // for (int i = 0; i < nums.size (); i++) {
