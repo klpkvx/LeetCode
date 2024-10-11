bcf846e38139c842c62eeda3dcbc33c7a3d99545
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 16:37:40 +0300

    Time: 3 ms (90.36%), Space: 8.7 MB (66.59%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size () != t.size ())
+        if (t.size () != s.size ())
             return false;
-        unordered_map<char, int> s_map;
-        unordered_map<char, int> t_map;
-        for (int i = 0; i < s.size (); i++) {
+        unordered_map<char, int> s_data; // s[i] -> index
+        unordered_map<char, int> t_data; // s[i] -> index
+        int sz = s.size ();
+        for (int i = 0 ; i < sz; i++) {
             char s_letter = s[i];
             char t_letter = t[i];
-            auto s_it = s_map.find (t_letter);
-            auto t_it = t_map.find (s_letter);
-            if (s_it == s_map.end () && t_it == t_map.end ()) {
-                s_map[t_letter] = i;
-                t_map[s_letter] = i;
-            } else if (s_it != s_map.end () && t_it == t_map.end () || t_it != t_map.end () && s_it == s_map.end ()) {
-                return false;
-            } else if (s_it->second != t_it->second)
+            auto s_it = s_data.find (s_letter);
+            auto t_it = t_data.find (t_letter);
+            if (s_it == s_data.end () && t_it == t_data.end ()) {
+                t_data[t_letter] = i;
+                s_data[s_letter] = i;
+            } else if (s_it != s_data.end () && t_it != t_data.end ()) {
+                if (s_it->second != t_it->second)
+                    return false;
+            } else
                 return false;
         }
         return true;
