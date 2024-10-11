16eea0b85ba29c78ce874ab554c47d26375ddfb9
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 23:18:39 +0300

    Time: 9 ms (59.69%), Space: 9.5 MB (71.97%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int x = 0, y = 0;
-        for (char move : moves){
-            switch (move){
-                case 'U': y++; break;
-                case 'D': y--; break;
-                case 'L': x++; break;
-                case 'R': x--; break;
-                default: continue;
-            }
+        int x = 0;
+        int y = 0;
+        for (char c : moves) {
+            if (c == 'U')
+                y++;
+            if (c == 'D')
+                y--;
+            if (c == 'L')
+                x--;
+            if (c == 'R')
+                x++;
         }
         return x == 0 && y == 0;
     }
