6279fb2c3596628d626e47796b8dc3345664aef4
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 18:32:39 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> n1_data;
-        for (int val : nums1) {
-            n1_data.insert (val);
-        }
+        unordered_set<int> n1_data {nums1.begin (), nums1.end ()};
+
         vector<int> answer;
         for (int val : nums2) {
-            if (n1_data.contains (val)) {
-                answer.push_back (val);
-                n1_data.erase (val);
-            }
+            if (n1_data.contains (val))
+                continue;
+            answer.push_back (val);
+            n1_data.erase (val);
         }
         return answer;
     }
