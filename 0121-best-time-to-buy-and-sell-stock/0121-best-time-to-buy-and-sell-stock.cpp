c92428da7631d58fc265c9acc4778d4ee6a43840
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 16:18:44 +0300

    Time: 98 ms (10.67%), Space: 96 MB (89.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int total_profit = 0;
         int price = prices[0];
-        int max_profit = 0;
         for (int i = 1; i < prices.size (); i++) {
             price = min (price, prices[i]);
-            max_profit = max (max_profit, prices[i] - price);
+            total_profit = max (total_profit, prices[i] - price);
         }
-        return max_profit;
+        return total_profit;
     }
 };
