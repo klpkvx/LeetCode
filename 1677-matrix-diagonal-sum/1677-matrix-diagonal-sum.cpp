9ce52ca75c0668c95e73265ae3e5a5f881253a6f
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 22:21:59 +0300

    Time: 7 ms (90.55%), Space: 14.1 MB (43.47%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int main_diag = 0;
-        int second_diag = 0;
+        int sum = 0;
         int sz = mat.size ();
         for (int i = 0; i < mat.size (); i++) {
-            main_diag += mat[i][i];
-            if (i != sz - 1 - i)
-            second_diag += mat[i][sz - 1 - i];
+            sum += mat[i][i];
+            sum += mat[i][sz - 1 - i];
         }
-        return main_diag + second_diag;
+        if (sz % 2 == 1) {
+            sum -= mat[sz /2][sz / 2];
+        }
+        return sum;
     }
 };
