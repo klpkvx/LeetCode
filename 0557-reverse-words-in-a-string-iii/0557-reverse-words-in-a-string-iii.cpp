8404d1be8b30e4b370d54571d7189d195b5b6a76
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 22:56:34 +0300

    Time: 16 ms (50.18%), Space: 165 MB (6.37%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void reverse_word (string &s, int left, int right){
-        while (left < right){
-            char tmp = s[left];
-            s[left] = s[right];
-            s[right] = tmp;
-            left++;
-            right--;
-        }
-    }
-
     string reverseWords(string s) {
-        int left = 0;
-        int right = 0;
-        while (right < s.size ()){
-            while (right < s.size () && s[right] != ' ') right++;
-            reverse_word (s, left, right - 1);
-            right++;
-            left = right;
+        string result;
+        for (int i = s.size () - 1; i >= 0; i--) {
+            string tmp;
+            while (i >= 0 && s[i] != ' ') {
+                tmp += s[i--];
+            }
+            if (!tmp.empty ()) {
+                if (result.empty ())
+                    result = tmp;
+                else {
+                    result = tmp + " " + result;
+                }
+            }
         }
-        return s;
+        return result;
     }
 };
