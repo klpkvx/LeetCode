85f404c230923289f0b9e8dc018bb2708342f953
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 17:22:33 +0300

    Time: 3 ms (37.68%), Space: 8.3 MB (95.27%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
+        if (nums.empty ())
+            return {};
         vector<string> answer;
+        int start = 0;
+        int end = 0;
         for (int i = 0; i < nums.size (); i++) {
             int start = i;
             int end = i;
-            
-            while (i + 1 < nums.size () && (long long)nums[i + 1] - (long long)nums[end] == 1) {
-                i++;
-                end = i;
-            }
 
-            answer.push_back (
-                             end - start == 0 ?
-                             to_string(nums[start]) : 
-                             to_string (nums[start]) + "->" + to_string (nums[end])
-                             );
+            while (i + 1 < nums.size () && abs ((long)nums[i] - (long)nums[i + 1]) == 1){
+                end++;
+                i++;
+            }
+            end = i;
+            answer.push_back (end - start >= 1 ? to_string(nums[start]) + "->" + to_string (nums[end])
+                                                : to_string (nums[start])
+            );
         }
         return answer;
     }
