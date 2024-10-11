ebc99f36c03ef374287bca5d62d6026fa0bc8710
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 00:21:34 +0300

    Time: 42 ms (48.12%), Space: 37.4 MB (23.83%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> n1_data;
-        unordered_set<int> n2_data;
-        for (int i = 0; i < nums1.size (); i++) {
-            n1_data.insert (nums1[i]);
+        unordered_set<int> n1_data{nums1.begin (), nums1.end ()};
+        unordered_set<int> n2_data{nums2.begin (), nums2.end ()};
+        vector<int> n1_values;
+        vector<int> n2_values;
+        for (int num : n1_data) {
+            if (!n2_data.contains (num))
+                n1_values.push_back (num);
         }
-
-        for (int j = 0; j < nums2.size (); j++) {
-            n2_data.insert (nums2[j]);
+        for (int num : n2_data) {
+            if (!n1_data.contains (num))
+                n2_values.push_back (num);
         }
-        vector<int> n1_answer;
-        vector<int> n2_answer;
-        for (int i = 0; i < nums2.size (); i++) {
-            if (!n1_data.contains (nums2[i])) {
-                n1_answer.push_back (nums2[i]);
-                n1_data.insert (nums2[i]);
-            }
-        }
-        for (int i = 0; i < nums1.size (); i++) {
-            if (!n2_data.contains (nums1[i])) {
-                n2_answer.push_back (nums1[i]);
-                n2_data.insert (nums1[i]);
-            }
-        }
-        return {n2_answer, n1_answer};
-
+        return {n1_values, n2_values};
     }
 };
