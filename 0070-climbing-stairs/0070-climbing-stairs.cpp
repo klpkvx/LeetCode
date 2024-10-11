8d0ccaa9694021c0f5f51452f375ab2baf32b9a6
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-11 12:51:11 +0300

    Time: 0 ms (100%), Space: 8.5 MB (9.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
 
-    int solve (int n, vector<int> &data){
+    unordered_map<int, int> data = {
+        {1, 1},
+        {2, 2}
+    }; // n -> sum
+    
+    int solve (int n) {
         if (n <= 0)
             return 0;
-
-        if (n == 1)
-            return 1;
-
-        if (n == 2)
-            return 2;
-
-        if (data[n] == -1)
-            data[n] = solve (n - 1, data) + solve (n - 2, data); 
-
-        return data[n];
+        if (data.contains (n))
+            return data[n];
+        data[n - 1] = solve (n - 1);
+        data[n - 2] = solve (n - 2);
+        return data[n - 1] + data[n - 2];
     }
 
-   int climbStairs (int n)
-    {
-        vector<int> data (n + 1, -1);
-
-        return solve (n, data);
+    int climbStairs(int n) {
+        return solve (n);
     }
 };
