195efed507e6688051a55aca53e24dbc8e1c7409
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 03:04:53 +0300

    Time: 24 ms (28.04%), Space: 12.7 MB (45.75%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> data; // sym -> freq
+        unordered_map<char, int> window; // char -> freq
         int left = 0;
         int max_len = 0;
         for (int right = 0; right < s.size (); right++) {
-            data[s[right]]++;
-            while (left < s.size () && data[s[right]] > 1) {
-                data[s[left]]--;
-                if (data[s[left]] <= 0) data.erase (s[left]);
+            window[s[right]]++;
+            while (left < s.size () && window[s[right]] > 1) {
+                window[s[left]]--;
+                if (window[s[left]] <= 0)
+                    window.erase (s[left]);
                 left++;
             }
-            max_len = max (max_len, right - left + 1);
+            max_len = max (max_len, (int)window.size ());
         }
         return max_len;
     }
