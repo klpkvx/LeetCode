f3bc39bc488a0e275f6a9d298e87fc09eacacf8d
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 03:40:41 +0300

    Time: 15 ms (17.69%), Space: 13.7 MB (30.37%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    vector<pair<string, int>> int_to_roman = {
-        {"M" , 1000},
-        {"CM",  900},
-        {"D" ,  500},
-        {"CD",  400},
-        {"C" ,  100},
-        {"XC",   90},
-        {"L" ,   50},
-        {"XL",   40},
-        {"X" ,   10},
-        {"IX",    9},
-        {"V" ,    5},
-        {"IV",    4},
-        {"I" ,    1}
+    vector<pair<string, int>> int_to_rom = {
+        {"M", 1000},
+        {"CM", 900},
+        {"D", 500},
+        {"CD", 400},
+        {"C", 100},
+        {"XC", 90},
+        {"L", 50},
+        {"XL", 40},
+        {"X", 10},
+        {"IX", 9},
+        {"V", 5},
+        {"IV", 4},
+        {"I", 1}
     };
-
     string intToRoman(int num) {
         string answer;
-        for (int i = 0; i < int_to_roman.size (); i++) {
-            while (num >= int_to_roman[i].second) {
-                num -= int_to_roman[i].second;
-                answer += int_to_roman[i].first;
+        for (int i = 0; i < int_to_rom.size(); i++) {
+            while (num >= int_to_rom[i].second) {
+                num -= int_to_rom[i].second;
+                answer += int_to_rom[i].first;
             }
         }
         return answer;
