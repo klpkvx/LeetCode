73790a809e05d57562946c06513959a3f63ff816
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 13:05:04 +0300

    Time: 0 ms (100%), Space: 14 MB (52.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,5 @@
 class Solution {
 public:
-//  l     m l m r
-// [4,5,6,7,0,1,2] | target = 0
     int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size () - 1;
@@ -13,13 +11,15 @@ public:
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
-                } else
+                } else {
                     left = mid + 1;
+                }
             } else {
                 if (nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
-                } else
+                } else {
                     right = mid - 1;
+                }
             }
         }
         return -1;
