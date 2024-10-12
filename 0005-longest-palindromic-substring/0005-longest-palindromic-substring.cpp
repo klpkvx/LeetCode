ad492aa474e427907f5f15f5e322a4af0651932a
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 03:30:20 +0300

    Time: 22 ms (61.22%), Space: 23.2 MB (45.14%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-
     string longestPalindrome(string s) {
-        const int sz = s.size ();
-        string result;
-        int max_len = -1;
-        for (int i = 0; i < sz; i++) {
-            auto check_substr = [&] (int left, int right) {
-                while (left >= 0 && right < sz && s[left] == s[right]) {
-                    if ( right - left + 1 > max_len) {
-                        max_len = right - left + 1;
-                        result = s.substr (left, right - left + 1);
+            int left = 0;
+            int right = s.size () - 1;
+            string answer;
+            int max_len = 0;
+            for (int i = 0; i < s.size (); i++) {
+                auto check_substr = [&] (int left, int right) -> void {
+                    while (left >= 0 && right < s.size () && s[left] == s[right]) {
+                            if (right - left + 1 > max_len) {
+                                max_len = right - left + 1;
+                                answer = s.substr (left, right - left + 1);
+                            }
+                        left--;
+                        right++;
                     }
-                    left--;
-                    right++;
-                }
-            };
-            check_substr (i, i);
-            check_substr (i, i + 1);
-        }
-        return result;
+                };
+                check_substr (i, i);
+                check_substr (i, i + 1);
+            }
+            return answer;
     }
 };
