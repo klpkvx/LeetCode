28896bc73c27aa87f91d844354d6a1d3786a61c8
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 03:33:25 +0300

    Time: 23 ms (60.37%), Space: 23.3 MB (44.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-            int left = 0;
-            int right = s.size () - 1;
             string answer;
             int max_len = 0;
             for (int i = 0; i < s.size (); i++) {
-                auto check_substr = [&] (int left, int right) -> void {
+                auto solve = [&] (int left, int right) {
                     while (left >= 0 && right < s.size () && s[left] == s[right]) {
-                            if (right - left + 1 > max_len) {
-                                max_len = right - left + 1;
-                                answer = s.substr (left, right - left + 1);
-                            }
+                        if (right - left + 1 > max_len) {
+                            max_len = right - left + 1;
+                            answer = s.substr (left, right - left + 1);
+                        }
                         left--;
                         right++;
                     }
                 };
-                check_substr (i, i);
-                check_substr (i, i + 1);
+                solve (i, i);
+                solve (i, i + 1);
             }
             return answer;
     }
