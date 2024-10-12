f20b52846404af9d637454760fa022433f039313
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 03:52:35 +0300

    Time: 0 ms (100%), Space: 12.4 MB (78.47%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    void generate (vector<string> &answer, string &curr, int n, int open, int close) {
-        if (curr.size () == n * 2) {
-            answer.push_back (curr);
-        }
-        if (open < n) {
-            curr.push_back ('(');
-            generate (answer, curr, n, open + 1, close);
-            curr.pop_back ();
+    vector<string> answer;
+    void solve (string &s, int open, int closed, int n) {
+        if (open == closed && open + closed == 2 * n) {
+            answer.push_back (s);
+            return;
         }
 
-        if (close < open) {
-            curr.push_back (')');
-            generate (answer, curr, n, open, close + 1);
-            curr.pop_back ();
+        if (open < n) {
+            s.push_back ('(');
+            solve (s, open + 1, closed, n);
+            s.pop_back ();
+        }
+
+        if (closed < open) {
+            s.push_back (')');
+            solve (s, open, closed + 1, n);
+            s.pop_back ();
         }
     }
 
     vector<string> generateParenthesis(int n) {
-        vector<string> answer;
-        string curr;
-        generate (answer, curr, n, 0, 0);
+        string s;
+        solve (s, 0, 0, n);
         return answer;
     }
 };
