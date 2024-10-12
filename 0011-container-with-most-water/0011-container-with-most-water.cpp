9a30e816a8f105c30f1ee34662c44023576f98eb
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-12 03:36:40 +0300

    Time: 59 ms (69.2%), Space: 61.7 MB (74.54%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left = 0;
-        int right = height.size () - 1;
-        int area = 0;
-        while (left <= right) {
+       int res = 0;
+       int left = 0;
+       int right = height.size () - 1;
+       while (left <= right)  {
             int h = min (height[left], height[right]);
-            int width = right - left;
-            area = max (area, h * width);
-            if (height[left] < height[right]) {
+            int w = right - left;
+            res = max (res, h * w);
+            if (height[left] < height[right])
                 left++;
-            } else {
+            else
                 right--;
-            }
-        }
-        return area;
+       }
+       return res;
     }
 };
