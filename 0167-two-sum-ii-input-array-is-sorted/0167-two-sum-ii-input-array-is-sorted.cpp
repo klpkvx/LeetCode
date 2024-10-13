d33a06deb77785ef7546ecedf790c380f0a06751
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 20:33:38 +0300

    Time: 11 ms (37%), Space: 18.3 MB (14%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,14 +4,12 @@ public:
         int left = 0;
         int right = nums.size () - 1;
         while (left <= right) {
-            int sum = nums[left] + nums[right];
-            if (sum < target) {
-                left++;
-            } else if (sum > target) {
-                right--;
-            } else {
+            if (nums[left] + nums[right] == target)
                 return {left + 1, right + 1};
-            }
+            if (nums[left] < target - nums[right])
+                left++;
+            if (nums[right] > target - nums[left])
+                right--;
         }
         return {};
     }
