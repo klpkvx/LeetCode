09fbf637fae2b057a9d1721333bd0e8a5808acb5
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 19:54:52 +0300

    Time: 24 ms (50.51%), Space: 22.9 MB (88.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    // 1 .. 2 .. 3 .. 5
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort (intervals.begin (), intervals.end ());
-
-        vector<vector<int>> answer = {intervals[0]};
+        vector<vector<int>> answer;
+        answer.push_back (intervals[0]);
         for (int i = 1; i < intervals.size (); i++) {
             if (intervals[i][0] <= answer.back ()[1]) {
-                answer.back ()[1] = max (intervals[i][1], answer.back ()[1]);
+                answer.back ()[1] = max (answer.back ()[1], intervals[i][1]);
             } else {
-                answer.push_back ({intervals[i][0], intervals[i][1]});
+                answer.push_back (intervals[i]);
             }
         }
         return answer;
