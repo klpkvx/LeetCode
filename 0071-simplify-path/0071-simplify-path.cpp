748508759c2550e675d09d575b914c52798dd027
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 19:51:32 +0300

    Time: 4 ms (72.11%), Space: 11.8 MB (43.9%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        string ans;
+        string answer;
         stack<string> st;
         for (int i = 0; i < path.size (); i++) {
+            string tmp;
             if (path[i] == '/')
                 continue;
-            string tmp;
+            
             while (i < path.size () && path[i] != '/')
                 tmp += path[i++];
+
             if (tmp == ".")
                 continue;
             else if (tmp == "..") {
                 if (!st.empty ())
                     st.pop ();
-            }
-            else
+            } else {
                 st.push (tmp);
+            }
         }
         while (!st.empty ()) {
-            ans = "/" + st.top () + ans;
+            answer = "/" + st.top () + answer;
             st.pop ();
         }
-        if (ans.empty ()) return "/";
-        return ans;
+        if (answer.empty ())
+            return "/";
+        return answer;
     }
 };
