563e5e813396f080e20112d4eefcd6e3db388cbb
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 20:37:21 +0300

    Time: 20 ms (95.94%), Space: 16.1 MB (85.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-
-    void solve(vector<vector<char>> &grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.size () || j >= grid[i].size () || grid[i][j] == '0')
+    
+    void dfs (vector<vector<char>> &grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.size () || j >= grid[i].size () || grid[i][j]=='0' || grid[i][j] == '3')
             return;
-        grid[i][j] = '0';
-        solve (grid, i + 1, j);
-        solve (grid, i - 1, j);
-        solve (grid, i, j + 1);
-        solve (grid, i, j - 1);
+
+        grid[i][j] = '3';
+        dfs (grid, i + 1, j);
+        dfs (grid, i - 1, j);
+        dfs (grid, i, j + 1);
+        dfs (grid, i, j - 1);
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int count = 0;
-        for (int i = 0; i <grid.size (); i++) {
+        for (int i = 0; i < grid.size (); i++) {
             for (int j = 0; j < grid[i].size (); j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    solve (grid, i, j);
+                    dfs (grid, i, j);
                 }
             }
         }
