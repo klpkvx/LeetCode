1fdc287396e740e3700cbbfa552b8b4e42c18791
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 23:53:54 +0300

    Time: 19 ms (37.94%), Space: 11.4 MB (34.54%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
 
-    bool is_same (const unordered_map<char,int> &data, const unordered_map<char,int> &p_data) {
-        for (auto &[key, val] : p_data) {
-            auto it = data.find (key);
+    bool is_same (unordered_map<char,int> &p_data, unordered_map<char, int> &data) {
+        for (auto &[elem, count] : p_data) {
+            auto it = data.find (elem);
             if (it == data.end ())
                 return false;
-            if (it->second != val)
+            if (it->second != count)
                 return false;
         }
         return true;
     }
 
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char, int> data;
-        unordered_map<char, int> p_data;
-        for (char c : p){
-            p_data[c]++;
-        }
         vector<int> answer;
+        unordered_map<char, int> p_data; // p[i] -> freq
+        for (int i = 0; i < p.size (); i++) {
+            p_data[p[i]]++;
+        }
+
         int left = 0;
+        unordered_map<char,int> data;
         for (int right = 0; right < s.size (); right++) {
             data[s[right]]++;
-            while (left < s.size () && right - left + 1 > p.size ()) { 
+            while (left < s.size () && right - left + 1 > p.size ()) {
                 data[s[left]]--;
                 left++;
             }
-            if (right - left + 1 == p.size () && is_same (data, p_data))
+
+            if (right - left + 1 == p.size () && is_same (p_data, data)) {
                 answer.push_back (left);
+            }
         }
-        return answer;        
+        return answer;
     }
 };
