c94f3f3744708a39c5ea604e37b9342ade5eaca8
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 23:00:55 +0300

    Time: 17 ms (16.05%), Space: 9.9 MB (8.17%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
 
-    int characterReplacement(string s, int k) {
-        unordered_map<char, int> data; // symbol -> frequency in substring
-        int left = 0;
-        int right = 0;
-        int max_len = 0;
-        int max_freq = 0;
-        for (int right = 0; right < s.size (); right++) {
-            data[s[right]]++;
-            max_freq = max (max_freq, data[s[right]]);
-            while (left < s.size () && (right - left + 1) - max_freq > k) {
-                data[s[left++]]--;
-            }
+    int most_freq (unordered_map<char,int>  &win) {
+        int mx = -1;
+        for (auto &[elem, val] : win)
+            mx =  max (mx, val);
+        return mx;
+    }
 
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> win; // s[i] -> freq
+        int left = 0;
+        int max_len = 0;
+        for (int right = 0; right < s.size (); right++) {
+            win[s[right]]++;
+            while (left < s.size () && right - left + 1 - most_freq (win) > k) {
+                win[s[left]]--;
+                if (win[s[left]] == 0)
+                    win.erase (s[left]);
+                left++;
+            }
             max_len = max (max_len, right - left + 1);
         }
         return max_len;
