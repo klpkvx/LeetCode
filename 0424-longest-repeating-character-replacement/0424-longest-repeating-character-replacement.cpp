6fdc6418c650e7b8fccaa1e49304502f2f4c72fa
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 23:02:29 +0300

    Time: 10 ms (49.09%), Space: 10 MB (6.72%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -12,10 +12,13 @@ public:
         unordered_map<char, int> win; // s[i] -> freq
         int left = 0;
         int max_len = 0;
+        int max_count = INT_MIN;
         for (int right = 0; right < s.size (); right++) {
             win[s[right]]++;
-            while (left < s.size () && right - left + 1 - most_freq (win) > k) {
+            max_count = max (max_count, win[s[right]]);
+            while (left < s.size () && right - left + 1 - max_count > k) {
                 win[s[left]]--;
+                max_count = max (max_count, win[s[left]]);
                 if (win[s[left]] == 0)
                     win.erase (s[left]);
                 left++;
