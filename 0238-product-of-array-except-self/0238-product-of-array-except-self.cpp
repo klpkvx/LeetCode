5115692510abecef5aab5b866905ed302a7b3601
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 21:16:46 +0300

    Time: 30 ms (38.34%), Space: 42 MB (5.13%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans (nums.size (), 1);
-        int left = 1;
-        for (int i = 0; i < nums.size (); i++) {
-            ans[i] *= left;
-            left *= nums[i];
+        int sz = nums.size ();
+        vector<int> prefix (sz + 1, 1);
+        vector<int> suffix (sz + 1, 1);
+        for (int i = 1; i < sz; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
         }
-        int right = 1;
-        for (int j = nums.size () - 1; j >= 0; j--) {
-            ans[j] *= right;
-            right *= nums[j];
+        for (int j = sz - 2; j >= 0; j--) {
+            suffix[j] = suffix[j + 1] * nums[j + 1];
         }
-        return ans;
+        vector<int> answer;
+        for (int i = 0; i < sz; i++) {
+            answer.push_back (suffix[i] * prefix[i]);
+        }
+        return answer;
     }
 };
-
-
-// OK, but it used memory
-        // vector<int> prefix_prod (nums.size () + 1, 1);
-        // vector<int> suffix_prod (nums.size () + 1, 1);
-        // for (int i = 1; i < nums.size (); i++) {
-        //     prefix_prod[i] = prefix_prod[i - 1] * nums[i - 1];
-        // }
-
-        // for (int j = nums.size () - 2; j >= 0; j--) {
-        //     suffix_prod[j] = suffix_prod[j + 1] * nums[j + 1];
-        // }
-        // vector<int> answer;
-        // for (int i =  0; i < nums.size (); i++) {
-        //     answer.push_back (prefix_prod[i] * suffix_prod[i]);
-        // }
-        // return answer;
-// TLE
-        // vector<int> answer (nums.size (), 1);
-        // for (int i = 0; i < nums.size (); i++) {
-        //     for (int j = 0; j < nums.size (); j++) {
-        //         if (i == j)
-        //             continue;
-        //         answer[i] *= nums[j];
-        //     }
-        // }
-        // return answer;
