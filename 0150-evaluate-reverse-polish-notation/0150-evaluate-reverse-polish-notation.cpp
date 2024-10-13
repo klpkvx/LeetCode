67feba01d014786abb75d16345f0ff733e1b319e
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-13 20:31:09 +0300

    Time: 3 ms (97.12%), Space: 15.9 MB (21.53%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
+    unordered_map<string, function<int (int, int)>> ops = {
+        {"+", [] (int a, int b) {return a + b; }},
+        {"-", [] (int a, int b) {return a - b; }},
+        {"*", [] (int a, int b) {return a * b; }},
+        {"/", [] (int a, int b) {return a / b; }}
+    };
     int evalRPN(vector<string>& tokens) {
         stack<int> values;
-        int answer = 0;
-        unordered_map<string, function<double (int, int)>> commands = {
-            {"+", [] (int a, int b) {return a + b;} },
-            {"*", [] (int a, int b) {return a * b;} },
-            {"-", [] (int a, int b) {return a - b;} },
-            {"/", [] (int a, int b) {return a / b;} },
-        };
-        for (int i = 0; i < tokens.size (); i++) {
-            const string &curr = tokens[i];
-            if (!commands.contains (curr)) {
-                values.push (stoi (curr));
+        for (const string &t : tokens) {
+            if (!ops.contains (t)) {
+                values.push (stoi (t));
             } else {
-                int right = values.top ();
-                values.pop ();
-                int left = values.top ();
-                values.pop ();
-                values.push (commands[curr] (left, right));
+                int right = values.top (); values.pop ();
+                int left = values.top (); values.pop ();
+                values.push (ops[t] (left, right));
             }
         }
-        return values.empty () ? 0 : values.top ();
-    }
+        return values.top ();
+    }  
 };
