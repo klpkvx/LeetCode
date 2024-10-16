24c3bc67e163e55ee6058cda35690a7b0a295b21
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-17 00:22:05 +0300

    Time: 206 ms (39.44%), Space: 148.1 MB (12.78%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> data = {{0, -1}}; // value -> index
+        unordered_map<int, int> data = {{0, -1}};
         int sum = 0;
         for (int i = 0; i < nums.size (); i++) {
             sum += nums[i];
-            int total = sum % k;
-            if (data.contains (total)) {
-                if (i - data[total] > 1)
+            int q = sum % k;
+            if (data.contains (q)) {
+                if (i - data[q] > 1)
                     return true;
-            } else {
-                data[total] = i;
             }
+            data.insert ({q, i});
         }
         return false;
     }
