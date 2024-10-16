9537fd47db70c7ede6ec9a308579b6bd4602fa92
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-17 00:12:28 +0300

    Time: 85 ms (51.74%), Space: 13.1 MB (68.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,17 +4,14 @@ public:
         vector<int> T (nums.size (), 1);
         for (int i = 1; i < nums.size (); i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
+                if (nums[j] < nums[i]) {
                     T[i] = max (T[i], T[j] + 1);
                 }
             }
         }
-        int max = 0;
-        for (int i = 0; i < T.size (); i++) {
-            if (T[i] > max) {
-                max = T[i];
-            }
-        }
-        return max;
+        int mx = 0;
+        for (int i = 0; i < T.size (); i++)
+            mx = max (mx, T[i]);
+        return mx;
     }
 };
