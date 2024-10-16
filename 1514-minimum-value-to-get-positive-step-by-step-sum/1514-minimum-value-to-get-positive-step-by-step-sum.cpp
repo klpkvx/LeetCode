a3b8f85e57b0574a50cc61c96fa93537593199a8
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-16 21:38:32 +0300

    Time: 4 ms (33.82%), Space: 9.3 MB (23.59%) - LeetHub

--- 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int min_value = 0;
         int count = INT_MAX;
-        vector<int> pref_sum (nums.size () + 1, 0);
-        for (int i = 1;  i < pref_sum.size (); i++) {
-            pref_sum[i] = pref_sum[i - 1] + nums[i - 1];
-            count = min (count, pref_sum[i]);
+        int sum = 0;
+        for (int i = 0;  i < nums.size (); i++) {
+            sum += nums[i];
+            count = min (count, sum);
         }
         if (count >= 1)
             return 1;
