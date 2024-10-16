dbfa16f110c36fb031f5327c593afa63d310d7a3
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-16 23:41:53 +0300

    Time: 67 ms (65.52%), Space: 13.2 MB (50.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-// 10 9 2 5 3 7 101 18
-// 1  1 1 1 1 1 1   1
-// 
-// if arr[j] < arr[i]
-//      T[i] = max (T[i], T[j] + 1)
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> ans (nums.size (), 1);
-        int mx = 0;
+        vector<int> T (nums.size (), 1);
         for (int i = 1; i < nums.size (); i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    ans[i] = max (ans[i], ans[j] + 1);
+                if (nums[i] > nums[j]) {
+                    T[i] = max (T[i], T[j] + 1);
                 }
             }
         }
-
-        for (int i = 0; i < ans.size (); i++) mx = max (mx, ans[i]);
-        return mx;
+        int max = 0;
+        for (int i = 0; i < T.size (); i++) {
+            if (T[i] > max) {
+                max = T[i];
+            }
+        }
+        return max;
     }
 };
