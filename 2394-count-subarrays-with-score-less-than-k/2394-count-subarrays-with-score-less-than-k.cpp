6f1ca94cafa9f17e37a7147606a19ea098078c5c
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-16 22:47:12 +0300

    Time: 108 ms (62.74%), Space: 97.9 MB (31.07%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        long long sum = 0;
-        long long left = 0;
+        int left = 0;
+        long long score = 0;
         long long count = 0;
-        for (long long right = 0; right < nums.size(); right++) {
-            sum += nums[right];
-            while (left < nums.size () && sum * (right - left + 1) >= k) {
-                sum -=nums[left];
+        for (int right = 0; right < nums.size (); right++) {
+            score += nums[right];
+            while (score * (right - left + 1)>= k){
+                score -= nums[left];
                 left++;
             }
             count += right - left + 1;
