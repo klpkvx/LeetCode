be755e1d4274753f4dc5579463177e8f4a396227
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-21 17:40:22 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.cpp
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        vector<int> answer;
-        int i = num.size () - 1;
         int carry = 0;
-        int digit = 0;
-        while (i >= 0 || k || carry) {
-            digit = (i >= 0 ? (num[i--]) : 0) + k % 10 + carry;
-            answer.push_back (digit % 10);
-            carry = digit / 10;
+        for (int i = num.size () - 1; i >= 0 && k > 0; i--) {
+            int digit = k % 10;
+            k /= 10;
+            int value = num[i] + digit + carry;
+            carry = value / 10;
+            num [i] = value % 10;
+        }
+
+        while (k > 0 || carry) {
+            int value = k % 10 + carry;
+            carry = value / 10;
+            num.insert (num.begin (), value % 10);
             k /= 10;
         }
-        reverse (answer.begin (), answer.end ());
-        return answer;
+
+        return num;
     }
 };
