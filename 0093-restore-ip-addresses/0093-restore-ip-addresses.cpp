547de6d84ce7cb2ef888590df29e055dfb5781bc
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-22 18:08:59 +0300

    Time: 0 ms (100%), Space: 8.9 MB (95.99%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        string tmp;
+        solve (0, 0, s, tmp);
+        return answer;
+    }
+private:
+    void solve (int dots, int index, string &s, string tmp) {
+
+        if (index == s.size () && dots == 4) {
+            tmp.erase (tmp.end () - 1);
+            answer.push_back (tmp);
+            return;
+        }
+
+        if (s.size () - index > (4 - dots) * 3)
+            return;
+
+        if (s.size () - index < 4 - dots)
+            return;
+
+        int num = 0;
+        for (int i = index; i < index + 3; i++) {
+            num = num * 10 + (s[i] - '0');
+            if (num <= 255) {
+                tmp += s[i];
+                solve (dots + 1, i + 1, s, tmp + '.');
+            }
+            if (num == 0)
+                break;
+        }
+    }
+private:
+    vector<string> answer;
+};
