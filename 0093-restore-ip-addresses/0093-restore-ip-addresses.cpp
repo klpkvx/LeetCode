0252b4e4579b48e83d118978737519359ceb46cd
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-23 00:49:39 +0300

    Time: 0 ms (100%), Space: 10.2 MB (73.13%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    vector<string> restoreIpAddresses(string s) {
-        string tmp;
-        solve (0, 0, s, tmp);
-        return answer;
-    }
-private:
-    void solve (int dots, int index, string &s, string tmp) {
 
-        if (index == s.size () && dots == 4) {
+    void solve (int dots, int index, string tmp, string &s) {
+        if (dots == 4 && index == s.size ()) {
             tmp.erase (tmp.end () - 1);
             answer.push_back (tmp);
             return;
         }
-
-        if (s.size () - index > (4 - dots) * 3)
+        if (dots > 4 || index >= s.size ()) {
             return;
-
-    //    if (s.size () - index < 4 - dots)
-    //        return;
-
+        }
         int num = 0;
         for (int i = index; i < index + 3; i++) {
-            num = num * 10 + (s[i] - '0');
+            num = num * 10 + s[i] - '0';
             if (num <= 255) {
                 tmp += s[i];
-                solve (dots + 1, i + 1, s, tmp + '.');
+                solve (dots + 1, i + 1, tmp + ".", s);
             }
             if (num == 0)
                 break;
         }
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        string tmp;
+        solve (0, 0, tmp, s);
+        return answer;
     }
 private:
     vector<string> answer;
