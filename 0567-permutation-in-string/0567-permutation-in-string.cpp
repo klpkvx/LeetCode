1d0ecb8b2e0152577c692c6f050f7961313b6626
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-27 23:10:05 +0300

    Time: 7 ms (52.99%), Space: 10.1 MB (31.37%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-
-    bool is_same (const unordered_map<char, int> &s1_data, const unordered_map<char, int> &s2_data) {
-        for (auto &[key, value] : s1_data) {
-            unordered_map<char,int>::const_iterator it = s2_data.find (key);
-            if (it == s2_data.end ())
+    bool is_same (const unordered_map<char, int> &reference, const unordered_map<char, int> &tmp) {
+        for (const auto &[letter, count] : reference) {
+            auto it = tmp.find (letter);
+            if (it == tmp.end ())
                 return false;
-            if (it->second != value)
+            if (it->second != count)
                 return false;
         }
         return true;
     }
-    bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> s1_data;
-        unordered_map<char, int> s2_data;
-        for (char c : s1) {
-            s1_data[c]++;
-        }
 
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> reference;
+        for (char c : s1) {
+            reference[c]++;
+        }
+        int window_sz = s1.size ();
         int left = 0;
+        unordered_map<char,int> tmp;
         for (int right = 0; right < s2.size (); right++) {
-            s2_data[s2[right]]++;
-            while (left < s2.size () && right - left + 1 > s1.size ()) {
-                s2_data[s2[left]]--;
+            tmp[s2[right]]++;
+            while (left < s2.size () && right - left + 1 > window_sz) {
+                tmp[s2[left]]--;
                 left++;
             }
-
-            if (right - left + 1 == s1.size () && is_same (s1_data, s2_data))
+            if (right - left + 1 == window_sz && is_same (reference, tmp))
                 return true;
-
         }
         return false;
     }
