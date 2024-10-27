de2a1b3d4707a0f5568be16d704cd1948f99e570
Anatoly Klepikov <82613059+klpkvx@users.noreply.github.com>
2024-10-28 00:16:47 +0300

    Time: 0 ms (100%), Space: 29.5 MB (58.99%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int left = -1, right = -1;
+        int left = - 1;
         for (int i = 0; i + 1 < nums.size (); i++) {
             if (nums[i] > nums[i + 1]) {
                 left = i;
                 break;
             }
         }
-
-        for (int i = nums.size () - 1; i >= 1; i--) {
-            if (nums[i] < nums[i - 1]) {
+        int right = -1;
+        for (int i = nums.size () - 1; i > 0; i--) {
+            if (nums[i] < nums [i - 1]) {
                 right = i;
                 break;
             }
         }
 
-        if (left == -1) return 0;
-        int min_i = nums[left], max_i = nums[left];
+        if (left == -1) 
+            return 0;
+
+        int min_i = nums[left];
+        int max_i = nums[left];
+
         for (int i = left; i <= right; i++) {
             min_i = min (min_i, nums[i]);
             max_i = max (max_i, nums[i]);
         }
-
         while (left > 0 && nums[left - 1] > min_i) {
             left--;
         }
-
-        while (right < nums.size () - 1 && nums[right + 1] < max_i) {
+        while (right + 1 < nums.size () && nums[right + 1] < max_i) {
             right++;
         }
         return right - left + 1;
