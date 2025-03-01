2d2741e1e9723651dc2516c0ca8f2f3e52a65816
klpkvx <82613059+klpkvx@users.noreply.github.com>
2025-03-01 10:37:41 +0300

    Time: 0 ms (100%), Space: 23.8 MB (50.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if (nums.empty () || nums.size () == 1)
-            return;
-            int ptr = 0;
+        int ptr = 0;
         for(int left = 0; left < nums.size (); left++) {
             if (nums[left] != 0)
                 nums[ptr++] = nums[left];
