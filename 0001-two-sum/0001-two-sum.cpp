68a47cbc2d621ab477008d5e8918d781ed95f1da
klpkvx <82613059+klpkvx@users.noreply.github.com>
2025-12-08 22:48:35 +0300

    Time: 5 ms (45.32%), Space: 15.1 MB (13.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> table; // nums[i] -> index
-        for (int i = 0; i < nums.size (); i++) {
-            int elem_to_search = target - nums[i];
-            if (table.contains (elem_to_search)) {
-                return {table[elem_to_search], i};
-            }
-            table[nums[i]] = i;
+        std::map<int, int> mm;
+        for (int i = 0; i < nums.size (); i++)
+        {
+            if (mm.contains (target - nums[i]))
+                return {i, mm[target-nums[i]]};
+            else
+                mm[nums[i]] = i;         
         }
         return {};
     }
