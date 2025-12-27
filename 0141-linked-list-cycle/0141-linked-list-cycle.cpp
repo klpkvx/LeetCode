ffa21993638aa9f1905b68a1b060a627011f604a
klpkvx <82613059+klpkvx@users.noreply.github.com>
2025-12-27 12:07:04 +0300

    Time: 13 ms (15.44%), Space: 11.9 MB (51.44%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,19 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        std::set<ListNode *> mm;
-        ListNode *curr = head;
-        while (curr)
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next)
         {
-            if (mm.contains (curr))
-            {
-              return true;
-            }
-            mm.insert (curr);
-            curr = curr->next;
-
+          slow = slow->next;
+          fast = fast->next->next;
+          if (slow == fast)
+            return true;
         }
-
         return false;
     }
 };
