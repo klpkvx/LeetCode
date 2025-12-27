4254ff6e8cfef09509ee1b0c84c9d7b7d1143da3
klpkvx <82613059+klpkvx@users.noreply.github.com>
2025-12-27 13:19:02 +0300

    Time: 4 ms (36.22%), Space: 10 MB (10.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size () != t.size ())
-            return false;
-        unordered_map<char,int> s_data; // s[i] -> freq;
-        for (int i = 0; i < s.size (); i++) {
-            s_data[s[i]]++;
-        }
-        for (int i = 0; i < t.size (); i++) {
-            if (s_data.contains (t[i]) && s_data[t[i]] > 0) {
-                s_data[t[i]]--;
-            } else 
+          return false;
+
+        std::unordered_map<char, int> mm;
+        for (const char l : s)
+            mm[l]++;
+        for (const char t : t)
+           mm[t]--;
+        for (const auto &[k, v] : mm)
+            if (v != 0)
                 return false;
-        }
+
         return true;
     }
 };
