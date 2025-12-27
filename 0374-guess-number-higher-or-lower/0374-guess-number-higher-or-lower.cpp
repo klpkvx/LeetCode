e5c53b608b960a0f6f07445b66f8f78b93238d33
klpkvx <82613059+klpkvx@users.noreply.github.com>
2025-12-27 12:41:10 +0300

    Time: 2600 ms (1.14%), Space: 7.8 MB (93.6%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -10,20 +10,14 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        long left = 0;
-        long right = n;
-        while (left <= right)
-        {
-            long candidate = (left + right) / 2;
-            long result = guess (candidate);
-            if (result == 0)
-                return candidate;
-            else if (result == -1)
-                right = candidate - 1;
-            else
-                left = candidate + 1;
-        }
+        long long int result = guess (n);
 
-        return 1337;        
+        if (result == 0)
+          return n;
+        
+        if (result == 1)
+          return guessNumber (n + 1);
+        else
+          return guessNumber (n - 1);  
     }
 };
