b27f4d5d49e21f99846b06be5f710f0333b86b67
klpkvx <82613059+klpkvx@users.noreply.github.com>
2025-12-27 12:58:13 +0300

    Time: 6 ms (20.54%), Space: 23.9 MB (11.92%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int r= 0;
-        for (int n : nums)
-            r ^= n;
-        return r;
+        std::unordered_map<int, int> mm; // num -> count
+        for (const int n : nums)
+        {
+            mm[n]++;
+        }
+        for (const auto &[k, v] : mm)
+        if (v == 1)
+          return k;
+        return -1;
     }
 };
